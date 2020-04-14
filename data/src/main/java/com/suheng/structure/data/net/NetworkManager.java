e5f7e7a4537dfb3e70654450fb61fbd0409fa238
ssywbj <weibj@zhipu-inc.com>
2020-04-14 11:25:27 +0800

    更改Structure后台服务器地址

--- data/src/main/java/com/suheng/structure/data/net/NetworkManager.java
+++ data/src/main/java/com/suheng/structure/data/net/NetworkManager.java
@@ -6,7 +6,8 @@ public class NetworkManager {
 
     public LoginTask doLoginRequest(String name, String pwd) {
         final LoginTask loginTask = new LoginTask(name, pwd);
-        loginTask.doRequest();
+        //loginTask.doRequest();
+        loginTask.doPostRequest();
         return loginTask;
     }
 
--- data/src/main/java/com/suheng/structure/data/net/URLConstants.java
+++ data/src/main/java/com/suheng/structure/data/net/URLConstants.java
@@ -1,10 +1,10 @@
 package com.suheng.structure.data.net;
 
 public class URLConstants {
-    //http://192.168.121.34:8080/SturctureServlet/Login?user_name=Wbj%E9%9F%A6%E5%B8%AE%E6%9D%B0&login_pwd=abc123
-    private static final String URL_SERVER = "http://192.168.121.34:8080";
+    //http://ip:8080/StructureServlet/Login?user_name=Wbj%E9%9F%A6%E5%B8%AE%E6%9D%B0&login_pwd=abc123
+    private static final String URL_SERVER = "http://192.168.121.56:8080";
     private static final String URL_STRUCTURE_SERVER = URL_SERVER + "/StructureService";
     public static final String URL_LOGIN_REQUEST = URL_STRUCTURE_SERVER + "/Login";
     //public static final String URL_LOGIN_REQUEST = "https://www.baidu.com/";
-    public static final String URL_GET_PICTURES_REQUEST = URL_STRUCTURE_SERVER + "/pictures";
+    public static final String URL_GET_PICTURES_REQUEST = URL_STRUCTURE_SERVER + "/Pictures";
 }
