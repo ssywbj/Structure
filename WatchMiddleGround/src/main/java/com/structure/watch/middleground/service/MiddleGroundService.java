778b3dbc6d182a4f9c37c2439ad55b8e37272528
ssywbj <weibj@zhipu-inc.com>
2020-04-21 20:49:09 +0800

    建立手表中台服务

---
+++ WatchMiddleGround/src/main/java/com/structure/watch/middleground/service/MiddleGroundService.java
@@ -0,0 +1,38 @@
+package com.structure.watch.middleground.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class MiddleGroundService extends Service {
+
+    private static final String TAG = MiddleGroundService.class.getSimpleName();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, TAG + ", onCreate");
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d(TAG, TAG + ", onBind");
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, TAG + ", onStartCommand");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, TAG + ", onDestroy");
+    }
+}
