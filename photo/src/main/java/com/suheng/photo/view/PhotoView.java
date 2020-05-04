edaaac32142e81a885956f9535f5b61f04ceb30d
ssywbj <weibj@zhipu-inc.com>
2020-05-05 01:29:52 +0800

    删除photo module

--- photo/src/main/java/com/suheng/photo/view/PhotoView.java
+++
@@ -1,9 +0,0 @@
-package com.suheng.photo.view;
-
-import com.suheng.photo.presenter.PhotoPresenter;
-import com.suheng.structure.ui.architecture.view.MvpView;
-
-public interface PhotoView extends MvpView<PhotoPresenter> {
-
-    void notifyDataSetChanged();
-}
