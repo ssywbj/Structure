73a1a5e19c3c5500f5ae877fba4f2e6b83d2cc02
ssywbj <weibj@zhipu-inc.com>
2020-05-06 17:51:28 +0800

    开始探究随机文件存储类

--- module1/src/main/java/com/suheng/structure/module1/Module1MainActivity.java
+++ module1/src/main/java/com/suheng/structure/module1/Module1MainActivity.java
@@ -1,14 +1,19 @@
 package com.suheng.structure.module1;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.suheng.structure.common.arouter.RouteTable;
-import com.suheng.structure.module1.request.StringTaskImpl;
-import com.suheng.structure.net.callback.OnFailureListener;
-import com.suheng.structure.net.callback.OnFinishListener;
 import com.suheng.structure.ui.architecture.basic.BasicActivity;
+
+import java.io.File;
 
 @Route(path = RouteTable.MODULE1_ATY_MODULE1_MAIN)
 public class Module1MainActivity extends BasicActivity {
@@ -21,7 +26,12 @@ public class Module1MainActivity extends BasicActivity {
         findViewById(R.id.text_test_string_task).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                StringTaskImpl stringTask = new StringTaskImpl();
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("*/*");//设置类型，我这里是任意类型，任意后缀的可以这样写。
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                startActivityForResult(intent, 1);
+
+                /*StringTaskImpl stringTask = new StringTaskImpl();
                 stringTask.addOnFinishListener(new OnFinishListener<String>() {
                     @Override
                     public void onFinish(String data) {
@@ -32,9 +42,40 @@ public class Module1MainActivity extends BasicActivity {
                     public void onFailure(int code, String errorMsg) {
 
                     }
-                });
+                });*/
             }
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+
+        if (requestCode == 1) {
+            if (data == null) {
+                showToast("data is null");
+                return;
+            }
+            Uri uri = data.getData();
+            if (uri == null) {
+                showToast("uri is null");
+                return;
+            }
+            String path = uri.getPath();
+            if (path == null) {
+                showToast("file path is null");
+                return;
+            }
+
+            File file = new File(path);
+            if (file.exists()) {
+                Toast.makeText(this, "文件路径：" + file.getPath(), Toast.LENGTH_SHORT).show();
+            } else {
+                showToast(file + ": is not is exists");
+            }
+        }
+    }
 }
