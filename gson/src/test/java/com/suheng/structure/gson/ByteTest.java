70f5bc3b8127241a73e537438304d3b71778b3be
ssywbj <weibj@zhipu-inc.com>
2020-05-19 11:20:41 +0800

    数值原码、反码与补码知识点

--- gson/src/test/java/com/suheng/structure/gson/ByteTest.java
+++ gson/src/test/java/com/suheng/structure/gson/ByteTest.java
@@ -35,12 +35,36 @@ public class ByteTest {
         char c = '龟';
         byte[] charArray = ByteUtil.intToByteArray(c);
         //char ch = 0x2EF3;//字符型与整型可互转
-        char ch = 0x31E3;//字符型与整型可互转
+        char ch = 0x31E3;//字符型与整型可互转，汉字Unicode编码范围：https://www.qqxiuzi.cn/zh/hanzi-unicode-bianma.php
         //charArray = ByteUtil.intToByteArray(ch);
-        //int ch = 65535+65536;//字符型与整型可互转
+        //int ch = 65535+65536;//字符型与整型可互转，Java一个汉字占几个字节：https://www.cnblogs.com/duguxiaobiao/p/9128817.html
         System.out.println("charArray: " + ByteUtil.byteArrayToHex(charArray) + ", number: " + ByteUtil.byteArrayToInt(charArray)
                 + ", ch: " + ch + "， charArray: " + ByteUtil.byteArrayToHex(ByteUtil.intToByteArray(ch)));
-        System.out.println("binary: " + Integer.toBinaryString(999) + ", " + Integer.bitCount(999));//bitCount：二进制表示中1的个数
+
+        //toBinaryString：用二进制表示数；bitCount：二进制表示中1的个数。
+        System.out.println("binary: " + Integer.toBinaryString(9) + ", " + Integer.bitCount(9));
+
+        int i = 257;
+        int j = i << 3;//<<：左移运算符，<<1相当于乘以2，<<2相当于乘以4，以此类推
+        int k = i >> 2;//>>：右移运算符，>>1相当于除以2，>>2相当于除以4，以此类推
+        System.out.println("i: " + i + ", j: " + j + ", k: " + k + "\n" + "i binary: " + Integer.toBinaryString(i)
+                + ", j binary: " + Integer.toBinaryString(j) + ", k binary: " + Integer.toBinaryString(k));
+
+        //当将一个int型强制类型转换为byte型的时候，最高的三个字节会被砍掉，只留下最低的8位赋值给byte型。
+        byte a = (byte) i;//1 2 4 8 16 32 64
+        byte d = (byte) j;
+        System.out.println("a: " + a + ", a binary: " + Integer.toBinaryString(a)
+                + ", d: " + d + ", d binary: " + Integer.toBinaryString(d));
+
+        //https://blog.csdn.net/chenchao2017/article/details/79733278?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-6.nonecase&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-6.nonecase
+        //在计算机中数值是以该数的补码形式存在的.https://blog.csdn.net/afsvsv/article/details/94553228
+        //原码：最高位表示符号位，"1"表示负号，"0"表示正号，其它位存放该数的二进制的绝对值，是最简单的机器数表示法。如10000010，最高位是1表示这是一个负数，
+        //其它位是0000010，值为2，所以10000010表示的是-2；同理00000010表示的是2。原码最大的问题就在于一个数加上他的相反数不等于零，如10000010+0000010是
+        //是不等于0的。
+
+        //反码：正数的反码等于原码；负数的反码等于原码除符号位外其它位按位取反，如：-2(10000010)的反码为11111101。
+
+        //补码：正数的补码等于原码，负数的补码等于反码+1，如：-2(10000010)的补码为11111110。或负数的补码等于原码自低位向高位，尾数的第一个‘1’及其右边的‘0’保持不变，左边的各位按位取反，符号位不变。
     }
 
     private byte[] makeCmd() {
