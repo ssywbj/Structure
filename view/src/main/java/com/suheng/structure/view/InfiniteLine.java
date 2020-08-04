46fa88860ae84edb6baa1b5ea7ca656739a4176e
ssywbj <1271829012@qq.com>
2020-08-05 02:56:55 +0800

    两个属性动画实现数字的水平滑动

--- view/src/main/java/com/suheng/structure/view/InfiniteLine.java
+++ view/src/main/java/com/suheng/structure/view/InfiniteLine.java
@@ -5,7 +5,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -15,13 +16,13 @@ import androidx.annotation.Nullable;
 
 public class InfiniteLine extends View {
     private Paint mPaint, mPaintText;
-    private Path mPath;
     private int mLineLen;
 
     private ValueAnimator mAnimator;
-    private float mAnimValue;
+    private int mAnimValue;
+    private ValueAnimator mAnimator2;
+    private int mAnimValue2;
     private boolean mNotStartAnim = true;
-    private int mPeriod = 0;
 
     public InfiniteLine(Context context) {
         this(context, null);
@@ -47,27 +48,49 @@ public class InfiniteLine extends View {
         mPaintText.setAntiAlias(true);
         mPaintText.setColor(Color.WHITE);
         mPaintText.setStyle(Paint.Style.FILL);
-        mPaintText.setTextSize(40f);
+        mPaintText.setTypeface(Typeface.DEFAULT_BOLD);
+        mPaintText.setTextSize(60f);
+        mPaintText.getTextBounds(text, 0, text.length(), mRectText);
+        Log.d("Wbj", "text len: " + mRectText.right);
 
-        mPath = new Path();
-
-        mAnimator = ValueAnimator.ofFloat(0, 0);
+        mAnimator = ValueAnimator.ofInt(0, 0);
+        final int duration = 5000;
         mAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
-                if (animation.getAnimatedValue() instanceof Float) {
-                    mAnimValue = (Float) animation.getAnimatedValue();
-                    long currentPlayTime = animation.getCurrentPlayTime();
-                    mPeriod = (int) (currentPlayTime / 1500);
-                    Log.d("Wbj", "onAnimationUpdate: " + mAnimValue + ", repeat count: " + currentPlayTime);
+                mAnimValue = (int) animation.getAnimatedValue();
+                Log.d("Wbj", "onAnimationUpdate: " + mAnimValue);
+                if (mAnimValue >= mRectText.right) {
+                    if (!mAnimator2.isRunning()) {
+                        mAnimator2.start();
+                        mAnimValue2 = 0;
+                    }
+                }
+                invalidate();
+            }
+        });
+        mAnimator.setDuration(duration);
+        mAnimator.setInterpolator(new LinearInterpolator());
+
+        mAnimator2 = ValueAnimator.ofInt(0, 0);
+        mAnimator2.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                mAnimValue2 = (int) animation.getAnimatedValue();
+                Log.d("Wbj", "mAnimValue2: " + mAnimValue2);
+                if (!mAnimator.isRunning()) {
                     invalidate();
+                }
+                if (mAnimValue2 >= mRectText.right) {
+                    if (!mAnimator.isRunning()) {
+                        mAnimator.start();
+                        mAnimValue = 0;
+                    }
                 }
             }
         });
-
-        mAnimator.setDuration(1500);
-        mAnimator.setInterpolator(new LinearInterpolator());
-        mAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        mAnimator2.setDuration(duration);
+        mAnimator2.setInterpolator(new LinearInterpolator());
     }
 
     @Override
@@ -78,28 +101,34 @@ public class InfiniteLine extends View {
         }
     }
 
+    private Rect mRectText = new Rect();
+    String text = " 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29";
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawColor(Color.BLACK);
 
-        /*mPath.reset();
-        mPath.moveTo(0, getHeight() / 2f);
-        mPath.lineTo(getWidth() - mOffsetX, getHeight() / 2f);
-        mPath.lineTo(getWidth() - mOffsetX + getWidth(), getHeight() / 2f);
-        canvas.drawPath(mPath, mPaint);*/
-
-        //canvas.drawTextOnPath("0", mPath, 0, 0, mPaintText);
-        canvas.drawText("" + mPeriod / 10 + mPeriod % 10, getWidth() - mAnimValue, getHeight() / 2f, mPaintText);
+        canvas.save();
+        canvas.translate(getWidth(), getHeight() / 2f);
+        if (mAnimator.isRunning()) {
+            canvas.drawText(text, -mAnimValue, 0, mPaintText);
+        }
+        if (mAnimator2.isRunning()) {
+            canvas.drawText(text, -mAnimValue2, 0, mPaintText);
+        }
+        canvas.restore();
 
         mLineLen = getWidth();
         if (mLineLen != 0) {
             if (mNotStartAnim) {
                 mNotStartAnim = false;
-                mAnimator.setFloatValues(0, mLineLen);
-                //mAnimator.start();
+                mAnimator.setIntValues(0, mRectText.right + getWidth());
+                mAnimator2.setIntValues(0, mRectText.right + getWidth());
+                mAnimator.start();
             }
         }
+
     }
 
 }
