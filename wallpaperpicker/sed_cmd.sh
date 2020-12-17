7e97b18003590f51f5a496bc4f0511343f6728e5
ssywbj <weibj@zhipu-inc.com>
2020-12-18 01:08:39 +0800

    sed按位置替换

--- wallpaperpicker/sed_cmd.sh
+++ wallpaperpicker/sed_cmd.sh
@@ -12,7 +12,6 @@ readonly new_version_code=$((version_code + 1))
 echo "new_version_code: $new_version_code"
 #sed -i '' 's/'"$version_code"'/'"$new_version_code"'/' $VERSION_FILE #mac
 #sed -i 's/'"$version_code"'/'"$new_version_code"'/' $VERSION_FILE
-#sed -i'' 's/'"$version_code"'/'"$new_version_code"'/' $VERSION_FILE
 #sed -i.bak 's/'"$version_code"'/'"$new_version_code"'/' $VERSION_FILE
 
 echo "------- 字符拆分：方法1 ------"
@@ -36,21 +35,23 @@ for i in "${array3[@]}"; do
   echo "$i"
 done
 
-echo "------- 字符与ASCII码互转 --------"
-i=65
-ch=$(echo $i | awk '{printf("%c", $1)}')
-echo "ch: $ch"
-((i++))
-ch=$(echo $i | awk '{printf("%c", $1)}')
-echo "ch: $ch"
+function basicCalc2() {
+  echo "------- 字符与ASCII码互转 --------"
+  i=65
+  ch=$(echo $i | awk '{printf("%c", $1)}')
+  echo "ch: $ch"
+  ((i++))
+  ch=$(echo $i | awk '{printf("%c", $1)}')
+  echo "ch: $ch"
 
-ascii=$(printf "%d" \'a)
-echo "ascii: $ascii"
-ascii=$(printf "%d" "'b")
-echo "ascii: $ascii"
+  ascii=$(printf "%d" \'a)
+  echo "ascii: $ascii"
+  ascii=$(printf "%d" "'b")
+  echo "ascii: $ascii"
+}
+#basicCalc2
 
 echo "------- 字母运算：a+0=a，a+1=b，... ，a+25=z;a代表0，b代表1，...，z代表25，逢26进1，如：a+26=ba -------"
-readonly SHIFT=26
 function basicCalc() {
   ascii=97 #ascii码97代表小写字母a
   ch=$(echo $ascii | awk '{printf("%c", $1)}')
@@ -65,9 +66,10 @@ function basicCalc() {
 #basicCalc
 
 function makeQuotMod() {
-  add=${1}
-  quot=$((add / SHIFT)) #商数
-  mod=$((add % SHIFT))  #余数
+  add=$1
+  shift=$2
+  quot=$((add / shift)) #商数
+  mod=$((add % shift))  #余数
 
   read -ra array <<<"$quot $mod"
   echo "${array[*]}"
@@ -80,33 +82,53 @@ function letterCalc() {
 }
 
 #array=($(makeQuotMod 0))
-read -ra array <<<"$(makeQuotMod 0)"
-echo "quot: ${array[0]}, mod: ${array[1]}"
-letterCalc "${array[1]}"
-read -ra array <<<"$(makeQuotMod 1)"
-echo "quot: ${array[0]}, mod: ${array[1]}"
-letterCalc "${array[1]}"
-read -ra array <<<"$(makeQuotMod 25)"
-echo "quot: ${array[0]}, mod: ${array[1]}"
-letterCalc "${array[1]}"
-read -ra array <<<"$(makeQuotMod 26)"
-echo "quot: ${array[0]}, mod: ${array[1]}"
-letterCalc "${array[1]}"
-read -ra array <<<"$(makeQuotMod 27)"
-echo "quot: ${array[0]}, mod: ${array[1]}"
-letterCalc "${array[1]}"
-read -ra array <<<"$(makeQuotMod 51)"
-echo "quot: ${array[0]}, mod: ${array[1]}"
-letterCalc "${array[1]}"
-read -ra array <<<"$(makeQuotMod 52)"
-echo "quot: ${array[0]}, mod: ${array[1]}"
-letterCalc "${array[1]}"
-read -ra array <<<"$(makeQuotMod 53)"
-echo "quot: ${array[0]}, mod: ${array[1]}"
-letterCalc "${array[1]}"
+#read -ra array <<<"$(makeQuotMod 0 26)"
+#echo "quot: ${array[0]}, mod: ${array[1]}"
+#letterCalc "${array[1]}"
+#read -ra array <<<"$(makeQuotMod 1 26)"
+#echo "quot: ${array[0]}, mod: ${array[1]}"
+#letterCalc "${array[1]}"
+#read -ra array <<<"$(makeQuotMod 25 26)"
+#echo "quot: ${array[0]}, mod: ${array[1]}"
+#letterCalc "${array[1]}"
+#read -ra array <<<"$(makeQuotMod 26 26)"
+#echo "quot: ${array[0]}, mod: ${array[1]}"
+#letterCalc "${array[1]}"
+#read -ra array <<<"$(makeQuotMod 27 26)"
+#echo "quot: ${array[0]}, mod: ${array[1]}"
+#letterCalc "${array[1]}"
+#read -ra array <<<"$(makeQuotMod 51 26)"
+#echo "quot: ${array[0]}, mod: ${array[1]}"
+#letterCalc "${array[1]}"
+#read -ra array <<<"$(makeQuotMod 52 26)"
+#echo "quot: ${array[0]}, mod: ${array[1]}"
+#letterCalc "${array[1]}"
+#read -ra array <<<"$(makeQuotMod 53 26)"
+#echo "quot: ${array[0]}, mod: ${array[1]}"
+#letterCalc "${array[1]}"
 
 #read -ra array <<<"${version_name//./ }"
-#echo "${array[*]}"
 #for var in "${array[@]}"; do
 #  echo "var: $var"
+#done
+
+cat tmp.txt
+sed 's/\.//g' tmp.txt
+#cat tmp.txt
+
+var=$(echo "1.0.0" | sed 's/\.//g')
+echo "var: $var"
+var=$(echo "$((var + 1))" | sed 's/\(.\)\(.\)/\1.\2./')
+echo "var: $var"
+var="100"
+echo "${var//\(.\)\(.\)/\1.\2.}"
+echo "var: $var"
+string="stirn.g"
+echo "${string//ir/ri}"
+echo "${string//./-}"
+var=$(echo "112234455960okcs" | sed 's/\(....\)\(...\)/\1:\2:/')
+echo "var: $var"
+#read -ra array <<<"${version_tmp//./ }"
+#for var in "${array[@]}"; do
+#  echo "var: $((var + 1))"
 #done
