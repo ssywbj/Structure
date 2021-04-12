b9e12c850a34972006d633b9a68d8b55f83a5796
ssywbj <weibj@zhipu-inc.com>
2021-04-12 18:19:58 +0800

    探究SurfaceView部分区域透明

--- view/src/main/java/com/suheng/structure/view/MainActivity.java
+++ view/src/main/java/com/suheng/structure/view/MainActivity.java
@@ -1,6 +1,7 @@
 package com.suheng.structure.view;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,6 +18,28 @@ public class MainActivity extends AppCompatActivity {
         imageView.setImageDrawable(drawable);
         drawable.start();
         Log.d("SVGView", "drawable, NumberOfFrames: " + drawable.getNumberOfFrames() + ", Duration: " + drawable.getDuration(0));*/
+
+        //动态开启或关闭：adb shell setprop log.tag.<YOUR_LOG_TAG> <LEVEL>，如：adb shell setprop log.tag.wbj_main D
+        if (Log.isLoggable("wbj_main", Log.INFO)) {
+            Log.i("wbj_main", "INFO, INFO, INFO");
+        }
+
+        if (Log.isLoggable("wbj_main", Log.WARN)) {
+            Log.w("wbj_main", "WARN, WARN, WARN");
+        }
+
+        Log.v("wbj_main", "VERBOSE, VERBOSE, VERBOSE");
+        Log.d("wbj_main", "DEBUG, DEBUG, DEBUG");
+        Log.i("wbj_main", "INFO, INFO, INFO");
+        Log.w("wbj_main", "WARN, WARN, WARN");
+        Log.e("wbj_main", "ERROR, ERROR, ERROR");
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Log.isLoggable("wbj_maine", Log.INFO)) {
+            Log.i("wbj_maine", "INFO-, INFO-, INFO-");
+        }
+    }
 }
