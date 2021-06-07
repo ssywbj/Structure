5b4abfd3cf9782965d4fa47ce7186c4f7f1426d3
Weibangjie <1271829012@qq.com>
2021-06-08 04:00:26 +0800

    平移阻尼

--- damping/src/main/java/com/suheng/damping/view/DampingView3.java
+++ damping/src/main/java/com/suheng/damping/view/DampingView3.java
@@ -2,20 +2,17 @@ package com.suheng.damping.view;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Color;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.TypedValue;
-import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Interpolator;
 import android.view.animation.TranslateAnimation;
 import android.widget.FrameLayout;
-import android.widget.TextView;
 
 import androidx.core.widget.NestedScrollView;
 
@@ -29,8 +26,7 @@ public class DampingView3 extends NestedScrollView {
 
     private int mMode;
 
-    private FrameLayout mLinearLayout;
-    private TextView mTextView;
+    private View mLayoutLoading;
     private int mLoadingViewHeight;
 
     public DampingView3(Context context) {
@@ -60,31 +56,26 @@ public class DampingView3 extends NestedScrollView {
     }
 
     private void init() {
-        if (mMode == 2) {
-            mLinearLayout = new FrameLayout(getContext());
-            //mLinearLayout.setOrientation(LinearLayout.VERTICAL);
-
-            mTextView = new TextView(getContext());
-            mTextView.setGravity(Gravity.CENTER);
-            mTextView.setTextColor(Color.RED);
-            mTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
-            mTextView.setBackgroundColor(Color.BLUE);
-            mTextView.setText("Loading");
-
-            mLoadingViewHeight = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 60, getResources().getDisplayMetrics());
-
-            DisplayMetrics displayMetrics = new DisplayMetrics();
-            getContext().getDisplay().getRealMetrics(displayMetrics);
-            mScreenHeight = displayMetrics.heightPixels;
-            Log.i(TAG, "screen height: " + mScreenHeight + ", mLoadingViewHeight: " + mLoadingViewHeight);
-        }
     }
 
+    private void calcHeights() {
+        mLoadingViewHeight = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP
+                , 70, getResources().getDisplayMetrics());
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getContext().getDisplay().getRealMetrics(displayMetrics);
+        mScreenHeight = displayMetrics.heightPixels;
+        Log.i(TAG, "screen height: " + mScreenHeight + ", mLoadingViewHeight: " + mLoadingViewHeight);
+    }
+
+    private static final float LOADING_START_ALPHA = 0.3f;
+    private static final float LOADING_DELTA_ALPHA = 1 - LOADING_START_ALPHA;
+    private static final float LOADING_START_SCALE = 0.5f;
+    private static final float LOADING_DELTA_SCALE = 1 - LOADING_START_SCALE;
+
     View childView;
-    private float mLoadingStartAlpha = 0.2f;
-    private float mLoadingEndAlpha = 1f, mLoadingDeltaAlpha = mLoadingEndAlpha - mLoadingStartAlpha;
-    private float mLoadingStartScale = 0.6f;
-    private float mLoadingEndScale = 1f, mLoadingDeltaScale = mLoadingEndScale - mLoadingStartScale;
+    private View mTextLoading;
+    private DampingProgressBar mProgressBar;
 
     @Override
     protected void onFinishInflate() {
@@ -93,22 +84,30 @@ public class DampingView3 extends NestedScrollView {
         if (getChildCount() == 1) {
             childView = getChildAt(0);
             if (mMode == 2) {
+                this.calcHeights();
+
                 final ViewGroup.LayoutParams layoutParams = childView.getLayoutParams();
                 removeView(childView);
 
-                //FrameLayout.LayoutParams params = new FrameLayout.LayoutParams();
-                mLinearLayout.addView(childView, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-                mLinearLayout.addView(mTextView, LayoutParams.MATCH_PARENT, mLoadingViewHeight);
-                //mTextView.setVisibility(GONE);
-                /*mTextView.setAlpha(mLoadingStartAlpha);
-                mTextView.setScaleX(mLoadingStartScale);
-                mTextView.setScaleY(mLoadingStartScale);*/
-                addView(mLinearLayout, layoutParams);
+                mLayoutLoading = inflate(getContext(), R.layout.damping_view_title, null);
+                FrameLayout layoutParent = new FrameLayout(getContext());
+                layoutParent.addView(childView, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+                layoutParent.addView(mLayoutLoading, LayoutParams.MATCH_PARENT, mLoadingViewHeight);
+                addView(layoutParent, layoutParams);
 
-                mDampingLayout = mLinearLayout;
+                mDampingLayout = layoutParent;
+
+                mTextLoading = findViewById(R.id.damping_text_loading);
+                mProgressBar = findViewById(R.id.damping_progress_bar);
+
+                mTextLoading.setScaleX(LOADING_START_SCALE);
+                mTextLoading.setScaleY(LOADING_START_SCALE);
+                mTextLoading.setAlpha(LOADING_START_ALPHA);
+
+                mProgressBar.setScaleX(LOADING_START_SCALE);
+                mProgressBar.setScaleY(LOADING_START_SCALE);
+                mProgressBar.setAlpha(LOADING_START_ALPHA);
             } else if (mMode == 1) {
-                mDampingLayout = childView;
-            } else {
                 mDampingLayout = childView;
             }
         }
@@ -117,91 +116,73 @@ public class DampingView3 extends NestedScrollView {
     int mTop;
 
     @Override
-    public void onAttachedToWindow() {
-        super.onAttachedToWindow();
-        Log.i(TAG, "onAttachedToWindow: " + getChildCount());
-    }
-
-    private int top;
-
-    @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         super.onLayout(changed, l, t, r, b);
-        if (mMode == 2) {
-            //scrollTo(0, mLoadingViewHeight);
-            //top = mDampingLayout.getTop();
-            mTop = top;
-            //mDampingLayout.layout(mDampingLayout.getLeft(), this.mTop - mLoadingViewHeight, mDampingLayout.getRight(), mDampingLayout.getBottom());
-            this.mTop = mDampingLayout.getTop();
-            Log.v(TAG, "onLayout: " + getChildCount() + ", mTop:" + mTop + ", top:" + top);
-            mTextView.setTranslationY(-mLoadingViewHeight);
+        if (mDampingLayout != null) {
+            mTop = mDampingLayout.getTop();
+            if (mMode == 2) {
+                //mLoadingViewHeight = 200;
+                mLayoutLoading.setTranslationY(-mLoadingViewHeight);
+            }
         }
-    }
-
-    /*@Override
-    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
-        super.onScrollChanged(l, t, oldl, oldt);
-        Log.i(TAG, "onScrollChanged, l: " + l + ", t: " + t + ", oldl: " + oldl + ", oldt: " + oldt);
-        if (t < mLoadingViewHeight) {
-            scrollTo(0, mLoadingViewHeight);
-        }
-    }*/
-
-    @Override
-    protected void onOverScrolled(int scrollX, int scrollY, boolean clampedX, boolean clampedY) {
-        super.onOverScrolled(scrollX, scrollY, clampedX, clampedY);
-        Log.d(TAG, "onOverScrolled, scrollXv: " + scrollX + ", scrollY: " + scrollY + ", clampedX: " + clampedX + ", clampedY: " + clampedY);
     }
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
-        //if (mMode == 1 || mMode == 2) {
-        this.calcDampingArea(ev);
-        //}
+        if (mDampingLayout != null) {
+            this.calcDampingArea(ev);
+        }
         return super.dispatchTouchEvent(ev);
     }
 
     private float mPreviousY;
     private float mMoveHeight;
     private int mScreenHeight;
-    float startY,distanceY;
+    float startY, distanceY;
 
     private void calcDampingArea(MotionEvent ev) {
-
         switch (ev.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 mPreviousY = ev.getY();
                 startY = mPreviousY;
                 mMoveHeight = 0;
-                //this.mTop = top;
                 break;
             case MotionEvent.ACTION_MOVE: //0.8 * pow(1 - x, 4), x=s/h, s是滑动距离、h是屏幕高度
                 float currentY = ev.getY();
-                 distanceY = Math.abs(currentY - startY);
+                //distanceY = Math.abs(currentY - startY);
                 float factor = (float) (0.8 * Math.pow(1 - 1.0 * distanceY / mScreenHeight, 4));
 
-                float deltaY = Math.abs(currentY - mPreviousY);
+                float deltaY = currentY - mPreviousY;
 
-                if (distanceY > mLoadingViewHeight) {
-                    Log.i(TAG, "滑了一个Loading的高度");
-                } else {
-                    mTextView.setTranslationY(-mLoadingViewHeight+distanceY);
-                    /*mTextView.setAlpha(mLoadingStartAlpha + mLoadingDeltaAlpha * distanceY / mLoadingViewHeight);
-                    mTextView.setScaleX(mLoadingStartScale + mLoadingDeltaScale * distanceY / mLoadingViewHeight);
-                    mTextView.setScaleY(mLoadingStartScale + mLoadingDeltaScale * distanceY / mLoadingViewHeight);*/
-                }
+                Log.d(TAG, mPreviousY + "--" + currentY + ", distance y: "
+                        + distanceY + ",factor: " + factor);
 
-                mMoveHeight += deltaY * factor;
+                //mMoveHeight += deltaY * factor;
+                mMoveHeight += deltaY;
 
                 if ((!canScrollVertically(-1)) && (currentY - mPreviousY) > 0) {
-                    Log.d(TAG, "scroll to up, " + mPreviousY + "--" + currentY + ", distance y: "
-                            + distanceY + ",factor: " + factor + ", top: " + mDampingLayout.getTop()
+                    Log.d(TAG, "scroll to up, top: " + mDampingLayout.getTop()
                             + ", bottom: " + mDampingLayout.getBottom() + ", move height: " + mMoveHeight);
-                    /*childView.layout(childView.getLeft(), (int) (mTop + mMoveHeight)
-                            , childView.getRight(), childView.getBottom());*/
-                    childView.setTranslationY(mMoveHeight);
-                }
+                    if (mLayoutLoading != null) {
+                        if (mMoveHeight <= 2.5 * mLoadingViewHeight) {
+                            if (mMoveHeight <= mLoadingViewHeight) {
+                                float percent = mMoveHeight / mLoadingViewHeight;
+                                mTextLoading.setScaleX(LOADING_START_SCALE + LOADING_DELTA_SCALE * percent);
+                                mTextLoading.setScaleY(mTextLoading.getScaleX());
+                                mTextLoading.setAlpha(LOADING_START_ALPHA + LOADING_DELTA_ALPHA * percent);
 
+                                mProgressBar.setScaleX(mTextLoading.getScaleX());
+                                mProgressBar.setScaleY(mTextLoading.getScaleX());
+                                mProgressBar.setAlpha(mTextLoading.getAlpha());
+                            }
+                            mLayoutLoading.setTranslationY(-mLoadingViewHeight + mMoveHeight);
+
+                            childView.setTranslationY(mMoveHeight);
+                        }
+                    }
+                    /*mDampingLayout.layout(mDampingLayout.getLeft(), (int) (mTop + mMoveHeight)
+                            , mDampingLayout.getRight(), mDampingLayout.getBottom());*/
+                }
 
                 /*if (canScrollVertically(1)) {
                 } else {
@@ -211,8 +192,12 @@ public class DampingView3 extends NestedScrollView {
                 mPreviousY = currentY;
                 break;
             case MotionEvent.ACTION_UP:
-                /*this.verticalAnimation();
-                childView.layout(childView.getLeft(), mTop, childView.getRight(), childView.getBottom());*/
+                childView.setTranslationY(0);
+                if (mLayoutLoading != null) {
+                    mLayoutLoading.setTranslationY(-mLoadingViewHeight);
+                }
+                //this.verticalAnimation();
+                //mDampingLayout.layout(mDampingLayout.getLeft(), mTop, mDampingLayout.getRight(), mDampingLayout.getBottom());
                 //mTextView.setTranslationY(-mLoadingViewHeight);
 
                 /*mTextView.setAlpha(mLoadingStartAlpha);
@@ -224,11 +209,11 @@ public class DampingView3 extends NestedScrollView {
 
     private void verticalAnimation() {
         TranslateAnimation animation = new TranslateAnimation(0.0f, 0.0f,
-                childView.getTop(), mTop); //上下回弹的动画效果
+                (int) (mTop + mMoveHeight), mTop); //上下回弹的动画效果
         animation.setDuration(600);
         animation.setFillAfter(true);
         animation.setInterpolator(new DampInterpolator());
-        childView.setAnimation(animation);
+        mDampingLayout.setAnimation(animation);
 
         /*TranslateAnimation animation1 = new TranslateAnimation(0.0f, 0.0f,
                 -mLoadingViewHeight+distanceY, -mLoadingViewHeight); //上下回弹的动画效果
