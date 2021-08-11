d340e2b96e41a7dc6a48a17fc6efe65a652c9c9e
ssywbj <1271829012@qq.com>
2021-08-12 04:57:48 +0800

    dfadfaf

---
+++ wallpaper/roamimg/src/main/java/com/suheng/wallpaper/roamimg/SsCity.java
@@ -0,0 +1,90 @@
+package com.suheng.wallpaper.roamimg;
+
+import java.io.Serializable;
+
+public class SsCity implements Serializable {
+    private String id;
+    private String name = "";
+    private String gmt;
+    private int isDefaultAndUnDelete = UN_DEFAULT_AND_UNDELETE;
+    private int offset;
+    private String Pinyin;
+
+    public static final int UN_DEFAULT_AND_UNDELETE = 0;
+
+    public SsCity() {
+    }
+
+    public SsCity(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        if (null == name) {
+            name = "";
+        }
+        return name;
+    }
+
+    public void setName(String name) {
+        if (null == name) {
+            name = "";
+        }
+        this.name = name;
+    }
+
+    public String getPinyin() {
+        return Pinyin;
+    }
+
+    public void setPinyin(String pinyin) {
+        Pinyin = pinyin;
+    }
+
+    public String getGmt() {
+        return gmt;
+    }
+
+    public void setGmt(String gmt) {
+        this.gmt = gmt;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    public int getIsDefaultAndUnDelete() {
+        return isDefaultAndUnDelete;
+    }
+
+    public void setIsDefaultAndUnDelete(int isDefaultAndUnDelete) {
+        this.isDefaultAndUnDelete = isDefaultAndUnDelete;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof SsCity && id != null && id.equals(((SsCity) o).getId())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
+    }
+
+}
---
+++ wallpaper/roamimg/src/main/java/com/suheng/wallpaper/roamimg/SsWorldTimeData.java
@@ -0,0 +1,73 @@
+package com.suheng.wallpaper.roamimg;
+
+import android.content.Context;
+import android.content.res.XmlResourceParser;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.TimeZone;
+
+public class SsWorldTimeData {
+    private static List<SsCity> mSearchList = null;
+
+    public static void clearSearchList() {
+        if (mSearchList == null) {
+            return;
+        }
+        mSearchList.clear();
+        mSearchList = null;
+    }
+
+    /**
+     * parser the xml to get the worldtime message
+     */
+    public static synchronized List<SsCity> getWorldTimeZones(Context context) {
+        if (null != mSearchList) {
+            return mSearchList;
+        }
+        List<SsCity> cityList = new ArrayList<>();
+        try {
+            XmlResourceParser xrp = context.getResources().getXml(R.xml.timezones);
+            while (xrp.getEventType() != XmlResourceParser.END_DOCUMENT) {
+                if (xrp.getEventType() == XmlResourceParser.START_TAG) {
+                    String tagName = xrp.getName();
+                    if (tagName.equals(SsWorldTimeConstants.XMLTAG_TIMEZONE)) {
+                        String id = xrp.getAttributeValue(0);
+                        String pinyin = xrp.getAttributeValue(1);
+                        String displayName = xrp.nextText();
+                        addListItem(cityList, id, displayName, pinyin);
+                    }
+                }
+                xrp.next();
+            }
+
+            xrp.close();
+        } catch (XmlPullParserException | IOException xppe) {
+            xppe.printStackTrace();
+        }
+
+        mSearchList = cityList;
+        return cityList;
+    }
+
+    private static void addListItem(List<SsCity> ssCities, String id, String displayName, String pinyin) {
+        final long date = Calendar.getInstance().getTimeInMillis();
+        SsCity city = new SsCity();
+        city.setId(id);
+        city.setName(displayName);
+        city.setPinyin(pinyin);
+        String tz_id = SsViewUtils.formatTimeZoneId(id);
+
+        String gmt = SsViewUtils.getCityGmt(tz_id);
+        final TimeZone tz = TimeZone.getTimeZone(tz_id);
+        final int offset = tz.getOffset(date);
+        city.setGmt(gmt);
+        city.setOffset(offset);
+        ssCities.add(city);
+    }
+
+}
