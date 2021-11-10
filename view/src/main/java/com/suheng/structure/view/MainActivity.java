c1bca77ab8752b75131f25921a392fb11c7606f8
Weibangjie <bangjie.wei@transsion.com>
2021-11-10 18:45:21 +0800

    dfafagdfaga

--- view/src/main/java/com/suheng/structure/view/MainActivity.java
+++ view/src/main/java/com/suheng/structure/view/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_slide_menu_layout);
 
         /*AnimationDrawable drawable = (AnimationDrawable) ContextCompat.getDrawable(this, R.drawable.map_my_location_img);
         //drawable.start();
--- view/src/main/java/com/suheng/structure/view/SlideMenuLayout.java
+++ view/src/main/java/com/suheng/structure/view/SlideMenuLayout.java
@@ -4,12 +4,11 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class SlideMenuLayout extends ViewGroup {
     public static final String TAG = SlideMenuLayout.class.getSimpleName();
-    private TextView mTextLeft, mTextRight;
 
     public SlideMenuLayout(Context context) {
         super(context);
@@ -41,40 +40,76 @@ public class SlideMenuLayout extends ViewGroup {
         return generateDefaultLayoutParams();
     }
 
+    private int mMenuWidth;
+
     //https://juejin.cn/post/6844904133489590279
     //https://academy.realm.io/posts/360andev-huyen-tue-dao-measure-layout-draw-repeat-custom-views-and-viewgroups-android/
     //方法执行顺序：onMeasure->onSizeChanged->onLayout，先measure得到尺寸后再layout方位
+    //https://github.com/ljphawk/SwipeMenuLayout.git
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        if (mTextLeft == null) {
-            mTextLeft = findViewById(R.id.text_left);
-            mTextRight = findViewById(R.id.text_right);
+        //heightMeasureSpec,xxdp：1073741944(但会随dp的多少而已改变),match_parent:1073743338,1073743334, wrap_content:-2147482134, -2147482138
+        //heightMode, xxdp、match_parent：MeasureSpec.EXACTLY(1073741824)，wrap_content：MeasureSpec.AT_MOST(-2147483648)
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        Log.i(TAG, "onMeasure: " + widthMeasureSpec + ", " + heightMeasureSpec);
+        Log.d(TAG, "onMeasure: " + MeasureSpec.EXACTLY + ", " + MeasureSpec.AT_MOST
+                + ", " + MeasureSpec.UNSPECIFIED + "===" + heightMode);
+        mMenuWidth = 0;
+        int childCount = getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = getChildAt(i);
+            if (i == 0) {
+                measureChildWithMargins(child, widthMeasureSpec, 0, heightMeasureSpec, 0); //先对View进行测量
+                MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams(); //对View进行测量后，才能拿到它的尺寸
+                int widthUsed = child.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
+                int heightUsed = child.getMeasuredHeight() + lp.topMargin + lp.bottomMargin;
+                setMeasuredDimension(widthUsed + getPaddingLeft() + getPaddingRight()
+                        , heightUsed + getPaddingTop() + getPaddingBottom());
+            } else {
+                /*if (heightMode == MeasureSpec.AT_MOST) {
+                    measureChild(child, widthMeasureSpec, heightMeasureSpec);
+                } else {
+                    measureChild(child, widthMeasureSpec, heightMeasureSpec);
+                }*/
+                measureChild(child, widthMeasureSpec, heightMeasureSpec);
+                mMenuWidth += child.getMeasuredWidth();
+                Log.d(TAG, "mMenuWidth: " + mMenuWidth);
+            }
         }
-        Log.d(TAG, "onMeasure: " + widthMeasureSpec + ", " + heightMeasureSpec);
-        //measureChild(mTextLeft, widthMeasureSpec, heightMeasureSpec); //XML中设置的Margin属性不算在测量范围内
-        //measureChildWithMargins：把在XML中设置的Margin属性算在测量范围内，让其生效
-        measureChildWithMargins(mTextLeft, widthMeasureSpec, 0, heightMeasureSpec, 0); //先对View进行测量
-        MarginLayoutParams lp = (MarginLayoutParams) mTextLeft.getLayoutParams(); //对View进行测量后，才能拿到它的尺寸
-        int widthUsed = mTextLeft.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
-
-        //measureChild(mTextRight, widthMeasureSpec, heightMeasureSpec);
-        measureChildWithMargins(mTextRight, widthMeasureSpec, widthUsed, heightMeasureSpec, 0);
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         Log.d(TAG, "onLayout: " + changed + ", " + l + ", " + t + ", " + r + ", " + b);
-        MarginLayoutParams layoutParams = (MarginLayoutParams) mTextLeft.getLayoutParams();
-        int x = getPaddingLeft() + layoutParams.leftMargin;
-        int y = getPaddingTop() + layoutParams.topMargin;
-        mTextLeft.layout(x, y, x += mTextLeft.getMeasuredWidth(), y + mTextLeft.getMeasuredHeight());
-        x += layoutParams.rightMargin;
+        int childCount = getChildCount();
+        int x = 0, y = 0;
+        int b1 = 0;
+        for (int i = 0; i < childCount; i++) {
+            View child = getChildAt(i);
+            if (i == 0) {
+                MarginLayoutParams layoutParams = (MarginLayoutParams) child.getLayoutParams();
+                x = getPaddingLeft() + layoutParams.leftMargin;
+                y = getPaddingTop() + layoutParams.topMargin;
+                b1 = y + child.getMeasuredHeight();
+                child.layout(x, y, x + child.getMeasuredWidth(), b1);
+                if (getLayoutDirection() == View.LAYOUT_DIRECTION_LTR) {
+                    x += child.getMeasuredWidth() - mMenuWidth;
+                }
+                //child.bringToFront();
+            } else {
+                //child.layout(x, y, x + child.getMeasuredWidth(), b1);
+                child.layout(x, y+10, x + child.getMeasuredWidth(), y + child.getMeasuredHeight()+10);
+                x += child.getMeasuredWidth();
+                //child.setVisibility(INVISIBLE);
+            }
+        }
 
-        layoutParams = (MarginLayoutParams) mTextRight.getLayoutParams();
-        x += getPaddingLeft() + layoutParams.leftMargin;
+        /*View child = getChildAt(0);
+        MarginLayoutParams layoutParams = (MarginLayoutParams) child.getLayoutParams();
+        x = getPaddingLeft() + layoutParams.leftMargin;
         y = getPaddingTop() + layoutParams.topMargin;
-        mTextRight.layout(x, y, x + mTextRight.getMeasuredWidth(), y + mTextRight.getMeasuredHeight());
+        child.layout(x, y, x + child.getMeasuredWidth(), y + child.getMeasuredHeight());*/
     }
 
     @Override
