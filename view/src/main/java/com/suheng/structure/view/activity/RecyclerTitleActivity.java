ee9d7636e92244d99ddf6eaf97bf13875a094061
Weibangjie <bangjie.wei@transsion.com>
2022-03-30 20:46:42 +0800

    MyItemAnimator

--- view/src/main/java/com/suheng/structure/view/activity/RecyclerTitleActivity.java
+++ view/src/main/java/com/suheng/structure/view/activity/RecyclerTitleActivity.java
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.suheng.structure.view.MyDefaultItemAnimator;
 import com.suheng.structure.view.R;
 import com.suheng.structure.view.adapter.RecyclerAdapter;
 
@@ -31,7 +32,9 @@ public class RecyclerTitleActivity extends AppCompatActivity {
 
         mRecyclerView = findViewById(R.id.recycler_title_rview);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
-        DefaultItemAnimator defaultItemAnimator = new DefaultItemAnimator();
+        MyDefaultItemAnimator defaultItemAnimator = new MyDefaultItemAnimator();
+        /*defaultItemAnimator.setAddDuration(2000);
+        defaultItemAnimator.setRemoveDuration(2000);*/
         mRecyclerView.setItemAnimator(defaultItemAnimator);
         ContentAdapter adapter = new ContentAdapter(mStringList);
 
@@ -39,9 +42,9 @@ public class RecyclerTitleActivity extends AppCompatActivity {
         mStringList.add(itemBean);
         mStringList.add(new ItemBean("PictureManager"));
         mStringList.add(new ItemBean("PictureTitle"));
-        mStringList.add(new ItemBean("LetterSelect"));
+        /*mStringList.add(new ItemBean("LetterSelect"));
         mStringList.add(new ItemBean("RecyclerView"));
-        mStringList.add(new ItemBean("AnimImageView"));
+        mStringList.add(new ItemBean("AnimImageView"));*/
 
         itemBean = new ItemBean(ItemBean.TYPE_TITLE, "标题2");
         mStringList.add(itemBean);
@@ -128,22 +131,25 @@ public class RecyclerTitleActivity extends AppCompatActivity {
 
                 RecyclerView.Adapter<?> adapter = mRecyclerView.getAdapter();
 
+                int index = 3;
                 if (adapter != null) {
                     if ("标题1".equals(data.getTitle())) {
-                        mStringList.add(2, new ItemBean("3333333333333"));
+                        /*mStringList.add(2, new ItemBean("3333333333333"));
                         mStringList.add(2, new ItemBean("2222222222222"));
                         mStringList.add(2, new ItemBean("1111111111111"));
-                        //adapter.notifyItemInserted(2);
-                        adapter.notifyItemRangeInserted(2, 3);
+                        adapter.notifyItemRangeInserted(2, 3);*/
+                        mStringList.add(index, new ItemBean("11111111111112222222222222222"));
+                        adapter.notifyItemInserted(index);
                     } else if ("标题2".equals(data.getTitle())) {
+                        /*mStringList.remove(2);
                         mStringList.remove(2);
                         mStringList.remove(2);
-                        mStringList.remove(2);
-                        //adapter.notifyItemRemoved(2);
-                        adapter.notifyItemRangeRemoved(2, 3);
+                        adapter.notifyItemRangeRemoved(2, 3);*/
+                        mStringList.remove(index);
+                        adapter.notifyItemRemoved(index);
                     }
 
-                    adapter.notifyItemRangeChanged(0, adapter.getItemCount());
+                    //adapter.notifyItemRangeChanged(0, adapter.getItemCount());
                 }
 
                 if (adapter instanceof RecyclerAdapter) {
