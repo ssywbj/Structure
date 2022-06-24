e094a9eaebd8237675d7ee392ebc0127f597bd6a
Weibangjie <bangjie.wei@transsion.com>
2022-06-24 19:33:33 +0800

    View HitRect

--- view/src/main/java/com/suheng/structure/view/activity/BlurActivity.java
+++ view/src/main/java/com/suheng/structure/view/activity/BlurActivity.java
@@ -3,6 +3,9 @@ package com.suheng.structure.view.activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Rect;
+import android.graphics.RenderEffect;
+import android.graphics.Shader;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.TouchDelegate;
@@ -22,35 +25,41 @@ public class BlurActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blur);
 
-        View rootLayout = findViewById(R.id.root_layout);
-        View parentDelegate = findViewById(R.id.layout_delegate);
+        View layoutTextDelegate = findViewById(R.id.layout_delegate);
         View textDelegate = findViewById(R.id.text_delegate);
         //https://www.codeleading.com/article/53194019795/
         //https://developer.android.com/guide/topics/renderscript/migrate#scripts
         //https://github.com/android/renderscript-intrinsics-replacement-toolkit
-        /*parentDelegate.setOnClickListener(new View.OnClickListener() {
+        View layoutTextDelegate3 = findViewById(R.id.layout_text_delegate3);
+        View textDelegate3 = findViewById(R.id.text_delegate3);
+        textDelegate3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(BlurActivity.this, "parentDelegate", Toast.LENGTH_SHORT).show();
+                Toast.makeText(BlurActivity.this, "Delegate2", Toast.LENGTH_SHORT).show();
             }
-        });*/
-        /*parentDelegate.post(new Runnable() {
+        });
+        layoutTextDelegate3.post(new Runnable() {
             @Override
             public void run() {
-                Rect outRect = new Rect();
-                parentDelegate.getHitRect(outRect);
-                Log.d("Wbj", "run: " + outRect.toShortString());
-                outRect.left += 200;
-                outRect.right -= 200;
-                Log.d("Wbj", "run: " + outRect.toShortString());
-                ((View) parentDelegate.getParent()).setTouchDelegate(new TouchDelegate(outRect, parentDelegate));
+                Rect childRect = new Rect();
+                textDelegate3.getHitRect(childRect); //获取点击热区，注：热区的位置是相对于父布局的
+                Rect parentRect = new Rect();
+                layoutTextDelegate3.getHitRect(parentRect);
+                Log.v("Wbj", "childRect: " + childRect.toString() + ", parentRect: " + parentRect.toString());
+
+                childRect.top = 0; //把textDelegate3的热区扩展到父布局的范围
+                childRect.left = 0;
+                childRect.right = parentRect.width();
+                childRect.bottom = parentRect.height();
+                Log.d("Wbj", "childRect: " + childRect.toString());
+                layoutTextDelegate3.setTouchDelegate(new TouchDelegate(childRect, textDelegate3));
             }
-        });*/
+        });
 
         textDelegate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(BlurActivity.this, "textDelegate1", Toast.LENGTH_SHORT).show();
+                Toast.makeText(BlurActivity.this, "Delegate1", Toast.LENGTH_SHORT).show();
             }
         });
         textDelegate.post(new Runnable() {
@@ -58,55 +67,21 @@ public class BlurActivity extends AppCompatActivity {
             public void run() {
                 Rect outRect = new Rect();
                 textDelegate.getHitRect(outRect);
-                Log.d("Wbj", "run: " + outRect.toShortString());
-                outRect.left -= 100;
-                outRect.right += 100;
-                Log.d("Wbj", "run: " + outRect.toShortString());
-                parentDelegate.setTouchDelegate(new TouchDelegate(outRect, textDelegate));
+                Log.d("Wbj", "outRect: " + outRect.toString());
+                outRect.inset(30, 30);  //验证热区向内缩进不生效，最小的热区就是View本身的尺寸，只能向外扩展生效
+                Log.d("Wbj", "outRect: " + outRect.toString());
+                layoutTextDelegate.setTouchDelegate(new TouchDelegate(outRect, textDelegate));
             }
         });
 
-        View textDelegate2 = findViewById(R.id.text_delegate2);
-        textDelegate2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(BlurActivity.this, "textDelegate2", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        View textDelegate3 = findViewById(R.id.text_delegate3);
-        textDelegate3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(BlurActivity.this, "textDelegate3", Toast.LENGTH_SHORT).show();
-            }
-        });
-        rootLayout.post(new Runnable() {
-            @Override
-            public void run() {
-                Rect outRect = new Rect();
-                textDelegate3.getHitRect(outRect);
-                Log.d("Wbj", "run: " + outRect.toShortString());
-                outRect.left -= 100;
-                outRect.right += 100;
-                Log.d("Wbj", "run: " + outRect.toShortString());
-                rootLayout.setTouchDelegate(new TouchDelegate(outRect, textDelegate3));
-            }
-        });
-
-        Toolkit toolkit = Toolkit.INSTANCE;
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.girl_gaitubao);
         ImageView imageView = findViewById(R.id.image_blur);
-
-        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             imageView.setRenderEffect(RenderEffect.createBlurEffect(20, 20, Shader.TileMode.MIRROR));
             imageView.setImageBitmap(bitmap);
         } else {
             imageView.setImageBitmap(Toolkit.INSTANCE.blur(bitmap, 20));
-        }*/
-
-        //imageView.setImageBitmap(bitmap);
-        imageView.setImageBitmap(toolkit.blur(bitmap, 20));
+        }
     }
 
 }
