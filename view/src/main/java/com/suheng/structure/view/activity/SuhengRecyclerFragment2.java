be156ba5183ce8e952ad6684bcb6973072b3be93
Weibangjie <bangjie.wei@transsion.com>
2022-07-18 19:33:19 +0800

    etw4ywryer

--- view/src/main/java/com/suheng/structure/view/activity/SuhengRecyclerFragment2.java
+++ view/src/main/java/com/suheng/structure/view/activity/SuhengRecyclerFragment2.java
@@ -36,7 +36,6 @@ import com.bumptech.glide.Glide;
 import com.google.android.renderscript.Toolkit;
 import com.suheng.structure.view.GridItemDecoration;
 import com.suheng.structure.view.R;
-import com.suheng.structure.view.adapter.RecyclerAdapter;
 import com.suheng.structure.view.utils.DateUtil;
 
 import java.nio.ByteBuffer;
@@ -48,7 +47,7 @@ public class SuhengRecyclerFragment2 extends SuhengBaseFragment {
     public static final int SPAN_COUNT = 3;
     public static final int SPAN_SPACE = 1;
     private ContentAdapter mContentAdapter;
-    private final List<ImageInfo> mDataList = new ArrayList<>();
+    private final List<Object> mDataList = new ArrayList<>();
     private RecyclerView mRecyclerView;
     private Context mContext;
 
@@ -95,7 +94,7 @@ public class SuhengRecyclerFragment2 extends SuhengBaseFragment {
         mRecyclerView.setLayoutManager(gridLayoutManager);
 
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
-        mRecyclerView.setAdapter(mContentAdapter = new ContentAdapter(mDataList));
+        mRecyclerView.setAdapter(mContentAdapter = new ContentAdapter());
 
         ActivityResultLauncher<String> activityResult = registerForActivityResult(new ActivityResultContracts.RequestPermission(),
                 result -> {
@@ -222,10 +221,13 @@ public class SuhengRecyclerFragment2 extends SuhengBaseFragment {
         //Cursor cursor = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, columns, null, null, sortOrder);
 
         String absolutePath = "/storage/emulated/0/DCIM/Camera/20161209_120251.jpg";//查询某一张图片
-        absolutePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath();//查询某个目录下的所有图片
+        absolutePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath();//查询某个目录下的图片
+        absolutePath = "";//查询所有目录下的图片
         String selection = MediaStore.Images.Media.DATA + " like ?";//查询条件
         String[] selectionArgs = {absolutePath + "%"};//查询目录
+
         Cursor cursor = mContext.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, columns, selection, selectionArgs, sortOrder); //columns传空表示查询所有字段
+        Log.d(TAG, "cursor.getCount(): " + cursor.getCount());
         if (cursor.moveToFirst()) {
             do {
                 String imagePath = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA));
@@ -246,39 +248,30 @@ public class SuhengRecyclerFragment2 extends SuhengBaseFragment {
         final int contentLength = imageInfoList.size();
         if (contentLength > 0) {
             boolean isNotSameDay;
-            List<ImageInfo> dateImages = null;
-            ImageInfo imageTitle = null;
+            //List<ImageInfo> dateImages = null;
+            TitleInfo imageTitle = null;
             /*
              * 算法：在for循环中，如果Item集合为空，那么先加入日期Title项再加入第一条数据项；往后的数据项与
              * 前一项比较，如果是同一日期则直接加在集合后面，如果是不同日期则先加入日期Title项再加入数据项。
              */
-            for (ImageInfo currentImage : imageInfoList) {
-                currentImage.setDate(DateUtils.formatDateTime(getContext(), currentImage.getDateModified(), DateUtils.FORMAT_SHOW_YEAR));
+            for (ImageInfo imageInfo : imageInfoList) {
+                imageInfo.setDate(DateUtils.formatDateTime(getContext(), imageInfo.getDateModified(), DateUtils.FORMAT_SHOW_YEAR));
                 if (mDataList.size() > 0) {
-                    isNotSameDay = !DateUtil.isSameDay(mDataList.get(mDataList.size() - 1).getDateModified()
-                            , currentImage.getDateModified());//mDataList.get(size - 1)：取出前一项数据
+                    isNotSameDay = !DateUtil.isSameDay(((ImageInfo) mDataList.get(mDataList.size() - 1))
+                            .getDateModified(), imageInfo.getDateModified());//mDataList.get(size - 1)：取出前一项数据
                 } else {
                     isNotSameDay = true;
                 }
                 if (isNotSameDay) { //如果不是同一天
-                    imageTitle = new ImageInfo(currentImage.getDate(), ContentAdapter.VIEW_TYPE_TITLE);
+                    imageTitle = new TitleInfo(imageInfo.getDate());
                     mDataList.add(imageTitle); //先加入日期Title项
-
-                    dateImages = new ArrayList<>();
-                    //mMapDateImage.put(currentImage.getDate(), dateImages);
                 }
-                if (imageTitle != null) {
-                    currentImage.setImageTitle(imageTitle);
-                }
-                mDataList.add(currentImage); //再加入数据项
-
-                if (dateImages != null) {
-                    dateImages.add(currentImage);
-                }
+                mDataList.add(imageInfo); //再加入数据项
             }
+
             imageInfoList.clear();
 
-            this.updateContentLength(contentLength);
+            mDataList.add(new FooterInfo(contentLength)); //再加入数据项
         } else {
             Log.d("Wbj", "没有数据了");
         }
@@ -287,97 +280,173 @@ public class SuhengRecyclerFragment2 extends SuhengBaseFragment {
         mContentAdapter.notifyDataSetChanged();
     }
 
-    private void updateContentLength(int contentLength) {
-        ImageInfo itemFooter = ImageInfo.getImageInfo().getItemFooter();
-        if (itemFooter == null) {
-            ImageInfo.getImageInfo().setItemFooter(new ImageInfo(contentLength, ContentAdapter.VIEW_TYPE_FOOTER));
-        } else {
-            ImageInfo.getImageInfo().getItemFooter().setContentLength(itemFooter.getContentLength() + contentLength);
-        }
-
-        itemFooter = ImageInfo.getImageInfo().getItemFooter();
-        mDataList.remove(itemFooter);
-        mDataList.add(itemFooter);
-
-        ImageInfo.getImageInfo().setContentLength(itemFooter.getContentLength());
-    }
-
     @Override
     public View getBlurredView() {
         return mRecyclerView;
     }
 
-    private class ContentAdapter extends RecyclerAdapter<ImageInfo, RecyclerAdapter.Holder> {
-        public static final int VIEW_TYPE_FOOTER = 1;
-        public static final int VIEW_TYPE_TITLE = 2;
+    private class ContentAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+        private static final int VIEW_TYPE_FOOTER = 1;
+        private static final int VIEW_TYPE_TITLE = 2;
 
-        public ContentAdapter(List<ImageInfo> dataList) {
-            super(dataList);
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            if (viewType == VIEW_TYPE_TITLE) {
+                return new TitleHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_picture_manager_adt_title, parent, false));
+            } else if (viewType == VIEW_TYPE_FOOTER) {
+                return new FooterHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_picture_manager_adt_footer, parent, false));
+            } else {
+                return new ContentHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_picture_manager_adt, parent, false));
+            }
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            Object object = mDataList.get(position);
+
+            if ((holder instanceof FooterHolder) && (object instanceof FooterInfo)) {
+                ((FooterHolder) holder).mTextCount.setText(getString(R.string.picture_bottom_number, ((FooterInfo) object).getCount()));
+            }
+
+            if ((holder instanceof TitleHolder) && (object instanceof TitleInfo)) {
+                final TitleHolder titleHolder = (TitleHolder) holder;
+                titleHolder.mTextDate.setText(((TitleInfo) object).getTitle());
+                titleHolder.mCheckBox.setVisibility(View.GONE);
+            }
+
+            if ((holder instanceof ContentHolder) && (object instanceof ImageInfo)) {
+                ContentHolder contentHolder = (ContentHolder) holder;
+                Glide.with(SuhengRecyclerFragment2.this).load(((ImageInfo) object).getPath()).into(contentHolder.mImageView);
+            }
         }
 
         @Override
         public int getItemViewType(int position) {
-            return getItem(position).getItemType();
-        }
-
-        @NonNull
-        @Override
-        public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            if (viewType == VIEW_TYPE_TITLE) {
-                return new TitleHolder(parent);
-            } else if (viewType == VIEW_TYPE_FOOTER) {
-                return new FooterHolder(parent);
+            Object object = mDataList.get(position);
+            if (object instanceof TitleInfo) {
+                return VIEW_TYPE_TITLE;
+            } else if (object instanceof FooterInfo) {
+                return VIEW_TYPE_FOOTER;
             } else {
-                return new ContentHolder(parent);
+                return 0;
             }
         }
 
         @Override
-        public void onBindViewHolder(@NonNull Holder holder, int pst, ImageInfo data) {
-            if (holder instanceof FooterHolder) {
-                ((FooterHolder) holder).mTextCount.setText(getString(R.string.picture_bottom_number, data.getContentLength()));
-            }
-
-            if ((holder instanceof TitleHolder)) {
-                final TitleHolder titleHolder = (TitleHolder) holder;
-                titleHolder.mTextDate.setText(data.getTitle());
-                titleHolder.mCheckBox.setVisibility(View.GONE);
-            }
-
-            if (holder instanceof ContentHolder) {
-                ContentHolder contentHolder = (ContentHolder) holder;
-                Glide.with(SuhengRecyclerFragment2.this).load(data.getPath()).into(contentHolder.mImageView);
-                //Glide.with(SuhengRecyclerFragment2.this).load(R.drawable.beauty2).into(contentHolder.mImageView);
-            }
+        public int getItemCount() {
+            return mDataList.size();
         }
     }
 
-    private final static class ContentHolder extends RecyclerAdapter.Holder {
+    private final static class ContentHolder extends RecyclerView.ViewHolder {
         ImageView mImageView;
 
-        ContentHolder(ViewGroup viewGroup) {
-            super(viewGroup, R.layout.activity_picture_manager_adt);
+        public ContentHolder(@NonNull View itemView) {
+            super(itemView);
             mImageView = itemView.findViewById(R.id.apma_image_view);
         }
     }
 
-    private final static class FooterHolder extends RecyclerAdapter.Holder {
+    private final static class FooterHolder extends RecyclerView.ViewHolder {
         TextView mTextCount;
 
-        FooterHolder(ViewGroup viewGroup) {
-            super(viewGroup, R.layout.activity_picture_manager_adt_footer);
+        public FooterHolder(@NonNull View itemView) {
+            super(itemView);
             mTextCount = itemView.findViewById(R.id.apmaf_text_count);
         }
     }
 
-    private final static class TitleHolder extends RecyclerAdapter.Holder {
+    private final static class TitleHolder extends RecyclerView.ViewHolder {
         TextView mTextDate;
         CheckBox mCheckBox;
 
-        TitleHolder(ViewGroup viewGroup) {
-            super(viewGroup, R.layout.activity_picture_manager_adt_title);
+        public TitleHolder(@NonNull View itemView) {
+            super(itemView);
             mTextDate = itemView.findViewById(R.id.apmat_text_date);
             mCheckBox = itemView.findViewById(R.id.checkbox);
+        }
+    }
+
+    private static class ImageInfo {
+        private final String path;
+        private final String dirName;
+        private final int width;
+        private final int height;
+        private final long size;
+        private final long dateModified;
+        private final int orientation;
+
+        private String date;
+
+        public ImageInfo(String path, String dirName, int width, int height, long size, long dateModified, int orientation) {
+            this.path = path;
+            this.dirName = dirName;
+            this.width = width;
+            this.height = height;
+            this.size = size;
+            this.dateModified = dateModified;
+            this.orientation = orientation;
+        }
+
+        public String getDate() {
+            return date;
+        }
+
+        public void setDate(String date) {
+            this.date = date;
+        }
+
+        public long getDateModified() {
+            return dateModified;
+        }
+
+        public int getOrientation() {
+            return orientation;
+        }
+
+        public String getPath() {
+            return path;
+        }
+
+        public String getDirName() {
+            return dirName;
+        }
+
+        public int getWidth() {
+            return width;
+        }
+
+        public int getHeight() {
+            return height;
+        }
+
+        public long getSize() {
+            return size;
+        }
+    }
+
+    private static class TitleInfo {
+        private final String title;
+
+        TitleInfo(String title) {
+            this.title = title;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+    }
+
+    private static class FooterInfo {
+        private final int count;
+
+        FooterInfo(int count) {
+            this.count = count;
+        }
+
+        public int getCount() {
+            return count;
         }
     }
 
