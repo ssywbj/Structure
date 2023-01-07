a8c71c75d0e889e7b213eb86bb7484ce480e26b1
Weibangjie <bangjie.wei@transsion.com>
2023-01-07 14:41:23 +0800

    MeasureSpec.UNSPECIFIED adjust

--- view/src/main/java/com/suheng/structure/view/PathKtView.kt
+++ view/src/main/java/com/suheng/structure/view/PathKtView.kt
@@ -8,8 +8,13 @@ import android.util.Log
 import android.view.View
 
 class PathKtView : View {
+    private object Singleton {
+        const val TAG = "PathKtView"
+    }
+
     private val mBitmap: Bitmap
     private val mRect = Rect()
+    private var mHeightMode: Int = 0
 
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
@@ -34,22 +39,19 @@ class PathKtView : View {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         //Log.v("Wbj", "mBitmap.width = ${mBitmap.width}, mBitmap.height = ${mBitmap.height}")
         val widthMode = MeasureSpec.getMode(widthMeasureSpec)
-        val heightMode = MeasureSpec.getMode(heightMeasureSpec)
+        mHeightMode = MeasureSpec.getMode(heightMeasureSpec)
         Log.v(
-            "PathKtView",
+            Singleton.TAG,
             "widthMeasureSpec = $widthMeasureSpec, heightMeasureSpec = $heightMeasureSpec," +
-                    " widthMode = $widthMode, heightMode = $heightMode"
+                    " widthMode = $widthMode, heightMode = $mHeightMode"
         )
-        Log.v(
-            "PathKtView",
+        /*Log.v(
+            Singleton.TAG,
             "MeasureSpec.AT_MOST = ${MeasureSpec.AT_MOST}, MeasureSpec.EXACTLY = ${MeasureSpec.EXACTLY}," +
                     " MeasureSpec.UNSPECIFIED = ${MeasureSpec.UNSPECIFIED}"
-        )
+        )*/
 
-        /*if (widthMode == MeasureSpec.AT_MOST && heightMode == MeasureSpec.AT_MOST) {
-            setMeasuredDimension(mBitmap.width, mBitmap.height) //固定宽高
-        }*/
-
+        //测量模式AT_MOST、EXACTLY、UNSPECIFIED，取决于其所在的父布局中对子View测量模式的定义
         var width = 0
         if (widthMode == MeasureSpec.AT_MOST) { //wrap_content
             width = mBitmap.width
@@ -57,30 +59,38 @@ class PathKtView : View {
             width = MeasureSpec.getSize(widthMeasureSpec)
         }
 
-        val height: Int = if (heightMode == MeasureSpec.EXACTLY) {
+        val height: Int = if (mHeightMode == MeasureSpec.EXACTLY) {
             MeasureSpec.getSize(heightMeasureSpec)
         } else {
-            //mBitmap.height
-            if (heightMode == MeasureSpec.AT_MOST) {
+            mBitmap.height
+            /*if (heightMode == MeasureSpec.AT_MOST) {
                 mBitmap.height
             } else {
+                //MeasureSpec.UNSPECIFIED：位于NestedScrollView、RecyclerView等可以上下滑动的ViewGroup中，
+                //因为可以上下滑动以展示全部的内容，所以不再限制子View的高度，让子View自己计算出相应的高度
                 MeasureSpec.getSize(heightMeasureSpec)
-            }
+            }*/
         }
 
-        Log.v("PathKtView", "width = $width, height = $height")
+        Log.v(Singleton.TAG, "width = $width, height = $height")
         setMeasuredDimension(width, height)
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-        mRect.set(0, 0, w, h)
+        Log.v(Singleton.TAG, "w = $w, h = $h, oldw = $oldw, oldh = $oldh")
+        if (mHeightMode == MeasureSpec.UNSPECIFIED) {
+            mRect.set(0, 0, mBitmap.width, h)
+        } else {
+            mRect.set(0, 0, w, h)
+        }
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        //canvas.drawBitmap(mBitmap, 0f, 0f, null)
+        canvas.save()
         canvas.drawBitmap(mBitmap, null, mRect, null)
+        canvas.restore()
     }
 
 }
