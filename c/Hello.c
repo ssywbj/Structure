50112d1d4865ef7af606a14919f177c129ce7c1d
ssywbj <1271829012@qq.com>
2023-02-28 02:37:20 +0800

    mac clang

--- c/Hello.c
+++ c/Hello.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-void main()
+int main()
 {
     printf("This is a C program.\n");
+
+    //Linux、Windows(需安装gcc)
     //编译命令:gcc Xxx.c, 生成文件a.out文件, 执行命令:./a.out
     //编译命令:gcc -o name Xxx.c, 生成name文件, 执行命令:./name
+
+    //Mac
+    //把gcc换成自带的clang命令
+
+    return 0;
 }
