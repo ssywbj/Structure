117e87249321a72b850820310b52fa9d0d7be91b
Weibangjie <bangjie.wei@transsion.com>
2023-02-28 14:04:51 +0800

    c struct

--- c/Hello.c
+++ c/Hello.c
@@ -2,10 +2,11 @@
 
 int main()
 {
+    //http://c.biancheng.net/view/1990.html
     printf("This is a C program.\n");
 
-    //Linux、Windows(需安装gcc)
-    //编译命令:gcc Xxx.c, 生成文件a.out文件, 执行命令:./a.out
+    //Linux、Windows(安装gcc)
+    //编译命令:gcc Xxx.c, 生成文件a.out(windows:a.exe), 执行命令:./a.out(windows:./a.exe)
     //编译命令:gcc -o name Xxx.c, 生成name文件, 执行命令:./name
 
     //Mac
