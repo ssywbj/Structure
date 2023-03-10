c9df228b1695a13cd5a3ef29f3aff98868c712b1
Weibangjie <bangjie.wei@transsion.com>
2023-03-10 14:36:18 +0800

    c insert sort222

--- c/algorithms/insert_sort.c
+++ c/algorithms/insert_sort.c
@@ -23,6 +23,24 @@ int main()
         arr[i + 1] = key;
     }
 
+    /*for(int j = 1; j < 3; j++)
+    {
+        int key = arr[j];
+        int i = 0;
+        while(i < j)
+        {
+            if(arr[i] > key)
+            {
+                arr[i + 1] = arr[i];
+            }
+
+            i++;
+            printf("i: %d, v: %d\n", i, arr[i]);
+        }
+
+        arr[i - 1] = key;
+    }*/
+
     for(int i = 0; i < len; i++)
     {
         printf("%d ", arr[i]);
