9c1f963bf5c07ea55de0b1425bbe181f294abc92
Weibangjie <bangjie.wei@transsion.com>
2023-12-01 12:56:12 +0800

    adfahfgdshdgfh

--- view/src/main/java/com/suheng/structure/view/wheel/CurvedImage.kt
+++ view/src/main/java/com/suheng/structure/view/wheel/CurvedImage.kt
@@ -15,6 +15,7 @@ import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.View
 import android.view.animation.LinearInterpolator
+import com.suheng.structure.view.BitmapHelper
 import com.suheng.structure.view.R
 import com.suheng.structure.view.kt.save
 
@@ -51,8 +52,12 @@ class CurvedImage @JvmOverloads constructor(
     }
     private var centerX = 1f
     private var centerY = 1f
-    private val rectTextIn: Rect = Rect()
+    private val rectTextIn: RectF = RectF()
     private val rectText: Rect = Rect()
+    private val rectBitmap: RectF = RectF()
+    private val bitmap7: Bitmap = BitmapHelper.get(getContext(), R.drawable.number_second_7, 0.4f)
+    private val bitmap0: Bitmap = BitmapHelper.get(getContext(), R.drawable.number_second_0, 0.4f)
+    private val bitmap70: Bitmap = BitmapHelper.mergeLeftRight(bitmap7, bitmap0)
 
     init {
         displayDensity = resources.displayMetrics.density
@@ -67,10 +72,15 @@ class CurvedImage @JvmOverloads constructor(
         val rectDimen =
             TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 40f, resources.displayMetrics)
                 .toInt()
-        rectTextIn.left = 30
-        rectTextIn.top = 30
+        rectTextIn.left = 30f
+        rectTextIn.top = 30f
         rectTextIn.right = rectTextIn.left + rectDimen
         rectTextIn.bottom = rectTextIn.top + rectDimen
+
+        rectBitmap.left = rectTextIn.right
+        rectBitmap.top = rectTextIn.top
+        rectBitmap.right = rectBitmap.left + bitmap70.width.toFloat() + 30
+        rectBitmap.bottom = rectBitmap.top + bitmap70.height.toFloat() + 30
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -112,15 +122,70 @@ class CurvedImage @JvmOverloads constructor(
 
         canvas.save {
             drawRect(rectTextIn, paintLine)
-            paintLine.color = Color.BLACK
-            val text = "48"
-            paintLine.getTextBounds(text, 0, text.length, rectText)
-            val textCenterX = rectTextIn.centerX().toFloat()
-            val textCentreY =
-                rectTextIn.top.toFloat() + rectText.height() + rectTextIn.height() / 2f
+            paintLine.color = Color.GRAY
+            drawLine(
+                rectTextIn.left,
+                rectTextIn.centerY(),
+                rectTextIn.right,
+                rectTextIn.centerY(),
+                paintLine
+            )
+            drawLine(
+                rectTextIn.centerX(),
+                rectTextIn.top,
+                rectTextIn.centerX(),
+                rectTextIn.bottom,
+                paintLine
+            )
 
             camera.save()
-            //camera.translate(0f, 0f, depthZ)
+            camera.translate(0f, 0f, -depthZ)
+            camera.rotateY(-depthZ)
+            camera.getMatrix(matrixCamera)
+            camera.restore()
+            /*floatArray.also {
+                matrixCamera.getValues(it)
+                it[6] = it[6] / displayDensity //数值修正
+                it[7] = it[7] / displayDensity //数值修正
+                matrixCamera.setValues(it)
+            }*/
+            matrixCamera.preTranslate(-rectTextIn.centerX(), -rectTextIn.centerY())
+            matrixCamera.postTranslate(rectTextIn.centerX(), rectTextIn.centerY())
+
+            val text = "48"
+            paintLine.getTextBounds(text, 0, text.length, rectText)
+            matrixCamera.preTranslate(rectTextIn.centerX(), rectTextIn.centerY())
+            concat(matrixCamera)
+            paintLine.color = Color.BLACK
+            drawText(
+                text,
+                -paintLine.measureText(text) / 2f,
+                -(paintLine.descent() + paintLine.ascent()) / 2f,
+                paintLine
+            )
+        }
+
+        canvas.save {
+            paintLine.color = Color.GRAY
+            drawRect(rectBitmap, paintLine)
+            paintLine.color = Color.RED
+            drawLine(
+                rectBitmap.left,
+                rectBitmap.centerY(),
+                rectBitmap.right,
+                rectBitmap.centerY(),
+                paintLine
+            )
+            drawLine(
+                rectBitmap.centerX(),
+                rectBitmap.top,
+                rectBitmap.centerX(),
+                rectBitmap.bottom,
+                paintLine
+            )
+
+            camera.save()
+            camera.translate(0f, 0f, depthZ)
             camera.rotateY(depthZ)
             camera.getMatrix(matrixCamera)
             camera.restore()
@@ -130,10 +195,12 @@ class CurvedImage @JvmOverloads constructor(
                 it[7] = it[7] / displayDensity //数值修正
                 matrixCamera.setValues(it)
             }
-            //matrixCamera.preTranslate(-textCenterX, -textCentreY)
-            matrixCamera.postTranslate(textCenterX, textCentreY)
+            matrixCamera.preTranslate(-rectBitmap.centerX(), -rectBitmap.centerY())
+            matrixCamera.postTranslate(rectBitmap.centerX(), rectBitmap.centerY())
+
+            matrixCamera.preTranslate(rectBitmap.centerX(), rectBitmap.centerY())
             concat(matrixCamera)
-            drawText(text, -rectText.width() / 2f, -rectText.height() / 2f, paintLine)
+            drawBitmap(bitmap70, -bitmap70.width / 2f, -bitmap70.height / 2f, null)
         }
 
         canvas.save {
