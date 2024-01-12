bc9980849fc618f899d22156647c0f66e8f92d4b
Weibangjie <bangjie.wei@transsion.com>
2024-01-12 18:41:19 +0800

    tuykghjkhjl

--- wallpaper/myhealth/src/main/java/com/suheng/wallpaper/myhealth/VapWallpaper.java
+++ wallpaper/myhealth/src/main/java/com/suheng/wallpaper/myhealth/VapWallpaper.java
@@ -1,8 +1,11 @@
 package com.suheng.wallpaper.myhealth;
 
+import android.app.KeyguardManager;
 import android.app.Presentation;
+import android.app.WallpaperManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.hardware.display.DisplayManager;
@@ -10,17 +13,28 @@ import android.hardware.display.VirtualDisplay;
 import android.service.wallpaper.WallpaperService;
 import android.util.Log;
 import android.view.SurfaceHolder;
+import android.view.TextureView;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.tencent.qgame.animplayer.AnimConfig;
 import com.tencent.qgame.animplayer.AnimView;
+import com.tencent.qgame.animplayer.inter.IAnimListener;
 import com.tencent.qgame.animplayer.util.ScaleType;
 
 public class VapWallpaper extends WallpaperService {
 
     private static final String TAG = VapWallpaper.class.getSimpleName();
+    private Context mContext;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        mContext = VapWallpaper.this;
         startService(new Intent(this, VapService.class));
     }
 
@@ -35,12 +49,109 @@ public class VapWallpaper extends WallpaperService {
         private VirtualDisplay virtualDisplay;
         private Presentation presentation;
         private AnimView animView;
+        private KeyguardManager mKeyguardManager;
+        private TextureView mTextureView;
+        private ImageView mImageView;
+
+        private WallpaperManager mWallpaperManager;
+
+        private final IAnimListener mAnimListener = new IAnimListener() {
+            @Override
+            public boolean onVideoConfigReady(@NonNull AnimConfig animConfig) {
+                return true;
+            }
+
+            @Override
+            public void onVideoStart() {
+                if (mTextureView == null) {
+                    View view = animView.getChildAt(0);
+                    if (view instanceof TextureView) {
+                        mTextureView = (TextureView) view;
+                    }
+                    Log.w(TAG, "mTextureView: " + mTextureView + ", thread: " + Thread.currentThread().getName());
+                }
+                //mImageView.setAlpha(0);
+                mImageView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        //mImageView.setImageAlpha(0);
+                        mImageView.setAlpha(0f);
+                        //mImageView.setBackgroundColor(Color.RED);
+                    }
+                });
+            }
+
+            @Override
+            public void onVideoRender(int frameIndex, @Nullable AnimConfig animConfig) {
+                final boolean keyguardLocked = mKeyguardManager.isKeyguardLocked();
+                final boolean isPreview = isPreview();
+                String msg = "onVideoRender, frameIndex: " + frameIndex + ", isKeyguardLocked: " + keyguardLocked
+                        + ", isPreview: " + isPreview;
+                if (mIsVisible) {
+                    if (keyguardLocked) {
+                        //Log.w(TAG, msg);
+                    } else {
+                        if (isPreview || animConfig == null) {
+                            return;
+                        }
+
+                        Log.v(TAG, msg);
+                        if (frameIndex == animConfig.getTotalFrames()) {
+                            if (mTextureView != null) {
+                                final Bitmap bitmap = mTextureView.getBitmap();
+                                Log.e(TAG, "bitmap:" + bitmap + ", thread:" + Thread.currentThread().getName());
+                                mImageView.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        //animView.stopPlay();
+                                        //mImageView.setBackground(new BitmapDrawable(getResources(), bitmap));
+                                        //mImageView.setImageBitmap(Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight()));
+                                        //mImageView.setImageAlpha(255);
+                                        mImageView.setAlpha(1.0f);
+                                        Log.w(TAG, "bitmap:" + bitmap + ", thread:" + Thread.currentThread().getName());
+                                    }
+                                });
+                            }
+
+                            //animView.stopPlay();
+                            //animView.setAlpha(0f);
+                        }
+                    }
+                } else {
+                    if (animView.isRunning()) {
+                        animView.stopPlay();
+                    }
+                }
+            }
+
+            @Override
+            public void onVideoComplete() {
+                if (mTextureView != null) {
+                    //Bitmap bitmap = mTextureView.getBitmap();
+                    //Log.w(TAG, "onVideoComplete, bitmap: " + bitmap);
+                }
+            }
+
+            @Override
+            public void onVideoDestroy() {
+                if (mTextureView != null) {
+                    //Bitmap bitmap = mTextureView.getBitmap();
+                    //Log.w(TAG, "onVideoDestroy, bitmap: " + bitmap);
+                }
+            }
+
+            @Override
+            public void onFailed(int i, @Nullable String s) {
+            }
+        };
 
         @Override
         public void onCreate(SurfaceHolder surfaceHolder) {
             super.onCreate(surfaceHolder);
             Log.d(TAG, "Engine, onCreate: " + this);
-            displayManager = (DisplayManager) VapWallpaper.this.getSystemService(Context.DISPLAY_SERVICE);
+            displayManager = (DisplayManager) mContext.getSystemService(Context.DISPLAY_SERVICE);
+            mKeyguardManager = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+            mWallpaperManager = WallpaperManager.getInstance(mContext);
         }
 
         @Override
@@ -60,12 +171,17 @@ public class VapWallpaper extends WallpaperService {
                 virtualDisplay.setSurface(holder.getSurface());
                 virtualDisplay.resize(width, height, densityDpi);
             }
-            presentation = new Presentation(VapWallpaper.this, virtualDisplay.getDisplay());
-            presentation.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-            presentation.setContentView(R.layout.vap_wallpaper);
-            animView = presentation.findViewById(R.id.animView);
-            animView.setLoop(Integer.MAX_VALUE);
-            animView.setScaleType(ScaleType.FIT_CENTER);
+            if (presentation == null) {
+                presentation = new Presentation(VapWallpaper.this, virtualDisplay.getDisplay());
+                Window window = presentation.getWindow();
+                if (window != null) {
+                    window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                }
+                presentation.setContentView(R.layout.vap_wallpaper);
+                animView = presentation.findViewById(R.id.animView);
+                animView.setScaleType(ScaleType.FIT_CENTER);
+                mImageView = presentation.findViewById(R.id.imageView);
+            }
         }
 
         @Override
@@ -84,10 +200,20 @@ public class VapWallpaper extends WallpaperService {
             }
         }
 
+        private boolean mIsVisible = false;
+
         @Override
         public void onVisibilityChanged(boolean visible) {
             super.onVisibilityChanged(visible);
-            Log.d(TAG, "onVisibilityChanged, visible = " + visible + ", " + this);
+            mIsVisible = visible;
+            final int lockWallpaperId = mWallpaperManager.getWallpaperId(WallpaperManager.FLAG_LOCK);
+            final boolean keyguardLocked = mKeyguardManager.isKeyguardLocked();
+            Log.i(TAG, "onVisibilityChanged, visible: " + visible
+                    + ", isKeyguardLocked: " + keyguardLocked
+                    + ", lockWallpaperId: " + lockWallpaperId
+                    + ", isKeyguardSecure: " + mKeyguardManager.isKeyguardSecure()
+                    + ", inKeyguardRestrictedInputMode: " + mKeyguardManager.inKeyguardRestrictedInputMode()
+            );
             if (virtualDisplay == null) {
                 return;
             }
@@ -96,7 +222,121 @@ public class VapWallpaper extends WallpaperService {
                     presentation.show();
                 }
                 if (animView != null) {
+                    animView.setAlpha(1f);
+                    if (keyguardLocked) {
+                        animView.setLoop(Integer.MAX_VALUE);
+                    } else {
+                        if (isPreview()) {
+                            animView.setLoop(Integer.MAX_VALUE);
+                        } else {
+                            animView.setLoop(1);
+                        }
+                    }
+                    animView.setAnimListener(new IAnimListener() {
+                        @Override
+                        public boolean onVideoConfigReady(@NonNull AnimConfig animConfig) {
+                            return true;
+                        }
+
+                        @Override
+                        public void onVideoStart() {
+                            if (mTextureView == null) {
+                                View view = animView.getChildAt(0);
+                                if (view instanceof TextureView) {
+                                    mTextureView = (TextureView) view;
+                                }
+                                Log.w(TAG, "mTextureView: " + mTextureView + ", thread: " + Thread.currentThread().getName());
+                            }
+                            //mImageView.setAlpha(0);
+                            mImageView.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    //mImageView.setImageAlpha(0);
+                                    mImageView.setAlpha(0f);
+                                    //mImageView.setBackgroundColor(Color.RED);
+                                }
+                            });
+                        }
+
+                        @Override
+                        public void onVideoRender(int frameIndex, @Nullable AnimConfig animConfig) {
+                            final boolean keyguardLocked = mKeyguardManager.isKeyguardLocked();
+                            final boolean isPreview = isPreview();
+                            String msg = "onVideoRender, frameIndex: " + frameIndex + ", isKeyguardLocked: " + keyguardLocked
+                                    + ", isPreview: " + isPreview;
+                            if (mIsVisible) {
+                                if (keyguardLocked) {
+                                    //Log.w(TAG, msg);
+                                } else {
+                                    if (isPreview || animConfig == null) {
+                                        return;
+                                    }
+
+                                    //Log.v(TAG, msg);
+                                    if (frameIndex == animConfig.getTotalFrames()) {
+                                        if (mTextureView != null) {
+                                            final Bitmap bitmap = mTextureView.getBitmap();
+                                            Log.e(TAG, "bitmap:" + bitmap + ", thread:" + Thread.currentThread().getName());
+                                            //if (bitmap != null) {
+                                                mImageView.post(new Runnable() {
+                                                    @Override
+                                                    public void run() {
+                                                        //animView.stopPlay();
+                                                        //mImageView.setBackground(new BitmapDrawable(getResources(), bitmap));
+                                                        if (bitmap != null) {
+                                                            //mImageView.setImageBitmap(Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight()));
+                                                            mImageView.setImageBitmap(bitmap);
+                                                        }
+                                                        //mImageView.setImageAlpha(255);
+                                                        //mImageView.setImageResource(R.drawable.shot_20240112_155330);
+                                                        mImageView.setAlpha(1.0f);
+                                                        animView.setAlpha(0f);
+                                                        Log.w(TAG, "bitmap:" + bitmap + ", thread:" + Thread.currentThread().getName());
+                                                    }
+                                                });
+                                            //}
+                                        }
+
+                                        animView.stopPlay();
+                                        //animView.setAlpha(0f);
+                                    }
+                                }
+                            } else {
+                                if (animView.isRunning()) {
+                                    animView.stopPlay();
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onVideoComplete() {
+                            if (mTextureView != null) {
+                                //Bitmap bitmap = mTextureView.getBitmap();
+                                //Log.w(TAG, "onVideoComplete, bitmap: " + bitmap);
+                            }
+                        }
+
+                        @Override
+                        public void onVideoDestroy() {
+                            if (mTextureView != null) {
+                                //Bitmap bitmap = mTextureView.getBitmap();
+                                //Log.w(TAG, "onVideoDestroy, bitmap: " + bitmap);
+                            }
+                        }
+
+                        @Override
+                        public void onFailed(int i, @Nullable String s) {
+                        }
+                    });
                     animView.startPlay(VapWallpaper.this.getAssets(), "demo.mp4");
+
+                    mImageView.setAlpha(0f);
+                    /*animView.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            animView.startPlay(VapWallpaper.this.getAssets(), "demo.mp4");
+                        }
+                    },200);*/
                 }
             } else {
                 if (animView != null) {
