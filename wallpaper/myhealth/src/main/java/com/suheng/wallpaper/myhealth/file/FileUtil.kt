5e4b8482a3777401cb27522d6ca68e5069a4dce6
bangjie.wei <bangjie.wei@transsion.com>
2024-10-10 21:35:46 +0800

    file md5 verify

--- wallpaper/myhealth/src/main/java/com/suheng/wallpaper/myhealth/file/FileUtil.kt
+++ wallpaper/myhealth/src/main/java/com/suheng/wallpaper/myhealth/file/FileUtil.kt
@@ -12,6 +12,7 @@ import java.io.FileOutputStream
 import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
+import java.security.MessageDigest
 
 object FileUtil {
 
@@ -63,22 +64,59 @@ object FileUtil {
         return data
     }
 
-    fun streamInputToFile(input: InputStream, dest: File): Result<Unit> {
-        return streamInputToOutput(input, FileOutputStream(dest))
+    fun copyFile(source: InputStream, dest: File): Result<Unit> {
+        return streamInputToOutput(source, FileOutputStream(dest))
     }
 
-    fun streamInputToFile(input: InputStream, dest: String): Result<Unit> {
-        return streamInputToOutput(input, FileOutputStream(dest))
+    fun copyFile(source: InputStream, dest: String): Result<Unit> {
+        return streamInputToOutput(source, FileOutputStream(dest))
     }
 
     fun copyFile(source: File, dest: File): Result<Unit> {
-        return streamInputToFile(FileInputStream(source), dest)
+        return copyFile(FileInputStream(source), dest)
     }
 
     fun copyFile(source: String, dest: String): Result<Unit> {
-        return streamInputToFile(FileInputStream(source), dest)
+        return copyFile(FileInputStream(source), dest)
     }
 
+    fun getMD5(file: File): Result<String> {
+        return kotlin.runCatching {
+            val md = MessageDigest.getInstance("MD5")
+            val inputStream = FileInputStream(file)
+            val buffer = ByteArray(4096)
+            var len = inputStream.read(buffer)
+            while (len != -1) {
+                md.update(buffer, 0, len)
+                len = inputStream.read(buffer)
+            }
+            inputStream.close()
+            bufferToHex(md.digest())
+        }
+    }
+
+    private fun bufferToHex(bytes: ByteArray): String {
+        return bufferToHex(bytes, 0, bytes.size)
+    }
+
+    private fun bufferToHex(bytes: ByteArray, m: Int, n: Int): String {
+        val sb = StringBuffer(2 * n)
+        val k = m + n
+        for (l in m until k) {
+            appendHexPair(bytes[l], sb)
+        }
+        return sb.toString()
+    }
+
+    private val hexDigits =
+        charArrayOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f')
+
+    private fun appendHexPair(bt: Byte, sb: StringBuffer) {
+        val c0 = hexDigits[bt.toInt() and 0xf0 ushr 4]
+        val c1 = hexDigits[bt.toInt() and 0x0f]
+        sb.append(c0)
+        sb.append(c1)
+    }
 }
 
 val coroutineScope = CoroutineScope(Dispatchers.Default + SupervisorJob())
--- wallpaper/myhealth/src/main/java/com/suheng/wallpaper/myhealth/repository/FileRepository.kt
+++ wallpaper/myhealth/src/main/java/com/suheng/wallpaper/myhealth/repository/FileRepository.kt
@@ -1,5 +1,6 @@
 package com.suheng.wallpaper.myhealth.repository
 
+import android.util.Log
 import com.suheng.wallpaper.myhealth.app.App
 import com.suheng.wallpaper.myhealth.file.FileUtil
 import kotlinx.coroutines.flow.flow
@@ -9,15 +10,25 @@ object FileRepository {
 
     private val context = App.appCtx()
     private const val FILE_NAME = "demo.mp4"
+    private const val FILE_MD5 = "3132824326bb07a1143739863e1e5762"
+    //private const val FILE_MD5 = "d993c3e9ecde8a1e73e7db01f36e2c0e"
     private val destFile = File(context.cacheDir.absolutePath + File.separator + FILE_NAME)
 
     fun loadVideoFile() = flow {
         if (destFile.exists()) {
-            emit(destFile)
-        } else {
-            if (FileUtil.streamInputToFile(context.assets.open(FILE_NAME), destFile).isSuccess) {
-                emit(destFile)
+            FileUtil.getMD5(destFile).onSuccess {
+                val isEqualsMD5 = (it == FILE_MD5)
+                Log.d("SimpleVapWallpaper", "file, md5: $it, isEqualsMD5: $isEqualsMD5")
+                if (isEqualsMD5) {
+                    emit(destFile)
+                } else {
+                    destFile.delete()
+                    FileUtil.copyFile(context.assets.open(FILE_NAME), destFile)
+                        .onSuccess { emit(destFile) }
+                }
             }
+        } else {
+            FileUtil.copyFile(context.assets.open(FILE_NAME), destFile).onSuccess { emit(destFile) }
         }
     }
 
