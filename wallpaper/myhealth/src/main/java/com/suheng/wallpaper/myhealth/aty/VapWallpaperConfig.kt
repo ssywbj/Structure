bf1771c328ccf056ca343388bb55471e54f389f1
bangjie.wei <bangjie.wei@transsion.com>
2024-10-23 12:26:16 +0800

    Compose Config

--- wallpaper/myhealth/src/main/java/com/suheng/wallpaper/myhealth/aty/VapWallpaperConfig.kt
+++ wallpaper/myhealth/src/main/java/com/suheng/wallpaper/myhealth/aty/VapWallpaperConfig.kt
@@ -2,7 +2,16 @@ package com.suheng.wallpaper.myhealth.aty
 
 import android.os.Bundle
 import android.widget.RadioGroup
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
 import com.suheng.wallpaper.myhealth.R
 import com.suheng.wallpaper.myhealth.file.PrefsUtils
 
@@ -10,6 +19,20 @@ class VapWallpaperConfig : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //setContentView()
+        setContent {
+            Text(
+                modifier = Modifier
+                    .height(15.dp)
+                    .background(Color.Green)
+                    .padding(horizontal = 6.dp),
+                text = "Hello Compose",
+                fontFamily = FontFamily.Cursive
+            )
+        }
+    }
+
+    private fun setContentView() {
         setContentView(R.layout.vap_wallpaper_config)
 
         findViewById<RadioGroup>(R.id.rg_render_way).apply {
