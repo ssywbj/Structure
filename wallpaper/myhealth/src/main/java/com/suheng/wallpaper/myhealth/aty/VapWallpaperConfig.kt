2d55624548a5a336091d0aadd7df13b37d002f3b
bangjie.wei <bangjie.wei@transsion.com>
2024-10-25 15:28:28 +0800

    Compose Item

--- wallpaper/myhealth/src/main/java/com/suheng/wallpaper/myhealth/aty/VapWallpaperConfig.kt
+++ wallpaper/myhealth/src/main/java/com/suheng/wallpaper/myhealth/aty/VapWallpaperConfig.kt
@@ -4,16 +4,25 @@ import android.os.Bundle
 import android.widget.RadioGroup
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.lifecycleScope
 import com.suheng.wallpaper.myhealth.R
+import com.suheng.wallpaper.myhealth.bean.AdtItem
 import com.suheng.wallpaper.myhealth.bean.asAdtItem
 import com.suheng.wallpaper.myhealth.file.PrefsUtils
 import com.suheng.wallpaper.myhealth.file.VideoLoader
@@ -28,26 +37,43 @@ class VapWallpaperConfig : AppCompatActivity() {
         //setContentView()
 
         val videoList = VideoLoader.getVideoList()
+        val itemList = mutableListOf<AdtItem>()
         if (videoList.isEmpty()) {
             lifecycleScope.launch(Dispatchers.IO) {
                 VideoRepository.parseVideoConfig().collect {
                     VideoLoader.setVideoList(it)
-                    it.map { video -> video.asAdtItem() }.forEach { item -> println(item) }
+                    itemList.addAll(it.map { video -> video.asAdtItem() }
+                        .onEach { item -> println(item) })
                 }
             }
         } else {
-            videoList.map { it.asAdtItem() }.forEach { println(it) }
+            itemList.addAll(videoList.map { it.asAdtItem() }.onEach { println(it) })
         }
 
         setContent {
-            Text(
-                modifier = Modifier
-                    .height(15.dp)
-                    .background(Color.Green)
-                    .padding(horizontal = 6.dp),
-                text = "Hello Compose",
-                fontFamily = FontFamily.Cursive
-            )
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(2.dp),
+                horizontalArrangement = Arrangement.spacedBy(2.dp),
+            ) {
+                itemsIndexed(itemList) { _, item ->
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        Image(
+                            painter = painterResource(item.preview),
+                            contentDescription = null,
+                            modifier = Modifier.fillMaxSize()
+                        )
+                        Text(
+                            item.name, modifier = Modifier
+                                .wrapContentSize()
+                                .align(Alignment.BottomCenter),
+                            Color.White, 18.sp, textAlign = TextAlign.Center
+                        )
+                    }
+
+                }
+            }
         }
     }
 
--- wallpaper/myhealth/src/main/java/com/suheng/wallpaper/myhealth/bean/Video.kt
+++ wallpaper/myhealth/src/main/java/com/suheng/wallpaper/myhealth/bean/Video.kt
@@ -7,11 +7,11 @@ data class Video(val id: Int = 0, val url: String, val path: String, val name: S
 
 data class AdtItem(val id: Int = 0, val name: String, @DrawableRes val preview: Int)
 
-inline fun <R> Video.mapItem(block: (Video) -> R): R = this.let(block)
+//inline fun <R> Video.mapItem(block: (Video) -> R): R = this.let(block)
 
 fun Video.previewResId() = when (id) {
     1 -> R.drawable.video2_preview
     else -> R.drawable.video1_preview
 }
 
-fun Video.asAdtItem() = mapItem { AdtItem(id, name, previewResId()) }
+fun Video.asAdtItem() = AdtItem(id, name, previewResId())
