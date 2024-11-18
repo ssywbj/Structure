f15ab71e4dca9d11f1f3960f95323dbfc4719866
bangjie.wei <bangjie.wei@transsion.com>
2024-11-18 20:41:27 +0800

    gl Matrix

--- opengl/src/main/java/com/suheng/opengl/renderer/MyRenderer2.java
+++ opengl/src/main/java/com/suheng/opengl/renderer/MyRenderer2.java
@@ -2,6 +2,7 @@ package com.suheng.opengl.renderer;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 import android.util.Log;
 
 import com.suheng.opengl.Utils;
@@ -16,12 +17,12 @@ import javax.microedition.khronos.opengles.GL10;
 
 public class MyRenderer2 implements GLSurfaceView.Renderer {
 
-    /*private static final String VERTEX_SHADER =
+    private static final String VERTEX_SHADER_MAT =
             "attribute vec4 vPosition;\n" +
             "uniform mat4 uMVPMatrix;\n" +
             "void main() {\n" +
             "  gl_Position = uMVPMatrix * vPosition;\n" +
-            "}";*/
+            "}";
     private static final String VERTEX_SHADER =
             "attribute vec4 vPosition;\n" +
             "void main() {\n" +
@@ -41,7 +42,7 @@ public class MyRenderer2 implements GLSurfaceView.Renderer {
     private final ShortBuffer mVertexIndexBuffer;
 
     private int mMatrixHandle;
-    private final float[] mMVPMatrix = new float[16];
+    private final float[] mMatrixProjection = new float[16];
 
     public MyRenderer2() {
         mVertexBuffer = ByteBuffer.allocateDirect(VERTEX_ANCHOR * 4)
@@ -55,12 +56,12 @@ public class MyRenderer2 implements GLSurfaceView.Renderer {
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        Log.d("Wbj", "onSurfaceCreated");
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
-        int program = GLES20.glCreateProgram();
-        int vertexShader = Utils.loadShader(GLES20.GL_VERTEX_SHADER, VERTEX_SHADER);
-        int fragmentShader = Utils.loadShader(GLES20.GL_FRAGMENT_SHADER, FRAGMENT_SHADER);
+        final int program = GLES20.glCreateProgram();
+        //final int vertexShader = Utils.loadShader(GLES20.GL_VERTEX_SHADER, VERTEX_SHADER);
+        final int vertexShader = Utils.loadShader(GLES20.GL_VERTEX_SHADER, VERTEX_SHADER_MAT);
+        final int fragmentShader = Utils.loadShader(GLES20.GL_FRAGMENT_SHADER, FRAGMENT_SHADER);
         GLES20.glAttachShader(program, vertexShader);
         GLES20.glAttachShader(program, fragmentShader);
         GLES20.glLinkProgram(program);
@@ -73,6 +74,7 @@ public class MyRenderer2 implements GLSurfaceView.Renderer {
                 12, mVertexBuffer);
 
         mMatrixHandle = GLES20.glGetUniformLocation(program, "uMVPMatrix");
+        Log.d("Wbj", "onSurfaceCreated, uMVPMatrix: " + mMatrixHandle);
     }
 
     @Override
@@ -80,31 +82,37 @@ public class MyRenderer2 implements GLSurfaceView.Renderer {
         GLES20.glViewport(0, 0, width, height);
 
         final int rectWidth = width / 2;
+        //final int rectWidth = width; //full view width
         final int rectHeight = rectWidth * 2 / 3;
         //final int rectHeight = rectWidth; //square
-        final float xAnchor = 1f * rectWidth / width; //width-based, width is px unit
-        //final float xAnchor = 1f; //full screen width
+        final float xAnchor = 1f * rectWidth / width; //x coordinate: width-based, width is px unit
         final float yAnchorRatio = xAnchor * rectHeight / rectWidth; //opengl coordinate height/width ratio
-        final float screenWHRatio = 1f * width / height; //screen width/height ratio
-        final float yAnchor = yAnchorRatio * screenWHRatio; //height-to-width ratio
+        final float screenWHRatio = 1f * width / height; //view width/height ratio
+        final float yAnchor = (mMatrixHandle != -1) ? yAnchorRatio : yAnchorRatio * screenWHRatio; //y coordinate
         Log.d("Wbj", "onSurfaceChanged, width: " + width + ", height: " + height
                 + ", rectWidth: " + rectWidth + ", rectHeight: " + rectHeight + ", xAnchor: " + xAnchor
                 + ", yAnchor: " + yAnchor + ", screenWHRatio: " + screenWHRatio
                 + ", rect w/h: " + (xAnchor / yAnchorRatio));
-        final float[] coordinates = {xAnchor, yAnchor, 0, -xAnchor, yAnchor, 0,
-                -xAnchor, -yAnchor, 0, xAnchor, -yAnchor, 0};
+        final float[] coordinates = {
+                xAnchor, yAnchor, 0,
+                -xAnchor, yAnchor, 0,
+                -xAnchor, -yAnchor, 0,
+                xAnchor, -yAnchor, 0
+        };
         mVertexBuffer.put(coordinates);
         mVertexBuffer.position(0);
 
-        //Matrix.perspectiveM(mMVPMatrix, 0, 45, (float) width / height, 0.1f, 100f);
-        //Matrix.translateM(mMVPMatrix, 0, 0f, 0f, -3.0f); //z可控制大小
+        if (mMatrixHandle != -1) {
+            Matrix.perspectiveM(mMatrixProjection, 0, 45, screenWHRatio, 0.1f, 100f);
+            Matrix.translateM(mMatrixProjection, 0, 0f, 0f, -5f); //z可控制大小
+
+            GLES20.glUniformMatrix4fv(mMatrixHandle, 1, false, mMatrixProjection, 0);
+        }
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-
-        //GLES20.glUniformMatrix4fv(mMatrixHandle, 1, false, mMVPMatrix, 0);
 
         GLES20.glDrawElements(GLES20.GL_TRIANGLES, mVertexIndexBuffer.capacity(), GLES20.GL_UNSIGNED_SHORT, mVertexIndexBuffer);
     }
