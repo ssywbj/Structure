75890b82156d7efe8c86ca4d54b4e19f7041a673
bangjie.wei <bangjie.wei@transsion.com>
2024-11-20 19:06:53 +0800

    thddghdghsrsgf

--- opengl/src/main/java/com/suheng/opengl/renderer/MyRenderer2.java
+++ opengl/src/main/java/com/suheng/opengl/renderer/MyRenderer2.java
@@ -11,6 +11,7 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 import java.nio.ShortBuffer;
+import java.util.Arrays;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -24,6 +25,7 @@ public class MyRenderer2 implements GLSurfaceView.Renderer {
 
     private RectRenderer mRectRenderer;
     private RectRenderer2 mRectRenderer2;
+    private RectRenderer3 mRectRenderer3;
 
     private int mWidth, mHeight;
 
@@ -37,6 +39,7 @@ public class MyRenderer2 implements GLSurfaceView.Renderer {
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         mRectRenderer = new RectRenderer();
         mRectRenderer2 = new RectRenderer2();
+        mRectRenderer3 = new RectRenderer3();
     }
 
     @Override
@@ -57,6 +60,8 @@ public class MyRenderer2 implements GLSurfaceView.Renderer {
         mRectRenderer2.onSurfaceCreated();
         mRectRenderer2.onSurfaceChanged(mWidth, mHeight);
         mRectRenderer2.onDrawFrame();
+
+        mRectRenderer3.onDrawFrame(mWidth, mHeight);
     }
 
     private static class RectRenderer  {
@@ -209,6 +214,148 @@ public class MyRenderer2 implements GLSurfaceView.Renderer {
         public void onDrawFrame() {
             GLES20.glDrawElements(GLES20.GL_TRIANGLES, mVertexIndexBuffer.capacity(), GLES20.GL_UNSIGNED_SHORT, mVertexIndexBuffer);
             GLES20.glDisableVertexAttribArray(mPositionHandle);
+        }
+    }
+
+    private static class RectRenderer3  {
+        private static final String VERTEX_SHADER_MAT =
+                "attribute vec4 vPosition;\n" +
+                "uniform mat4 uMVPMatrix;\n" +
+                "void main() {\n" +
+                "  gl_Position = uMVPMatrix * vPosition;\n" +
+                "}";
+        private static final String FRAGMENT_SHADER =
+                "precision mediump float;\n" +
+                "void main() {\n" +
+                "  gl_FragColor = vec4(0, 0, 1, 1);\n" +
+                "}";
+        private static final String FRAGMENT_SHADER2 =
+                "precision mediump float;\n" +
+                        "void main() {\n" +
+                        "  gl_FragColor = vec4(0, 1, 1, 1);\n" +
+                        "}";
+        private final FloatBuffer mVertexBuffer;
+        private final ShortBuffer mVertexIndexBuffer;
+        private final int mProgram;
+        private final int mProgram2;
+        private final float[] mMatrixProjection = new float[16];
+
+        public RectRenderer3() {
+            mVertexBuffer = ByteBuffer.allocateDirect(VERTEX_ANCHOR * 4)
+                    .order(ByteOrder.nativeOrder()).asFloatBuffer();
+            final float[] coordinates = {
+                    0.5f, 0.5f, 0,
+                    -0.5f, 0.5f, 0,
+                    -0.5f, -0.5f, 0,
+                    0.5f, -0.5f, 0
+            };
+            mVertexBuffer.put(coordinates);
+            mVertexBuffer.position(0);
+
+            mVertexIndexBuffer = ByteBuffer.allocateDirect(VERTEX_INDEX.length * 2)
+                    .order(ByteOrder.nativeOrder()).asShortBuffer();
+            mVertexIndexBuffer.put(VERTEX_INDEX);
+            mVertexIndexBuffer.position(0);
+
+            final int vertexShader = Utils.loadShader(GLES20.GL_VERTEX_SHADER, VERTEX_SHADER_MAT);
+            final int fragmentShader = Utils.loadShader(GLES20.GL_FRAGMENT_SHADER, FRAGMENT_SHADER);
+            mProgram = GLES20.glCreateProgram();
+            GLES20.glAttachShader(mProgram, vertexShader);
+            GLES20.glAttachShader(mProgram, fragmentShader);
+            GLES20.glLinkProgram(mProgram);
+
+            final int fragmentShader2 = Utils.loadShader(GLES20.GL_FRAGMENT_SHADER, FRAGMENT_SHADER2);
+            mProgram2 = GLES20.glCreateProgram();
+            GLES20.glAttachShader(mProgram2, vertexShader);
+            GLES20.glAttachShader(mProgram2, fragmentShader2);
+            GLES20.glLinkProgram(mProgram2);
+            Log.i("Wbj", "RectRenderer3(), program: " + mProgram + " , mProgram2: " + mProgram2);
+        }
+
+        /*public void onSurfaceCreated() {
+        }
+
+        public void onSurfaceChanged() {
+        }*/
+
+        public void onDrawFrame(int width, int height) {
+            GLES20.glUseProgram(mProgram);
+
+            final int positionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
+            GLES20.glEnableVertexAttribArray(positionHandle);
+            GLES20.glVertexAttribPointer(positionHandle, 3, GLES20.GL_FLOAT, false,12, mVertexBuffer);
+
+            final int rectWidth = width / 2;
+            //final int rectWidth = width; //full view width
+            final int rectHeight = rectWidth * 2 / 3;
+            //final int rectHeight = rectWidth; //square
+            final float screenWHRatio = 1f * width / height; //view width/height ratio
+            Log.d("Wbj", "onDrawFrame, width: " + width + ", height: " + height
+                    + ", rectWidth: " + rectWidth + ", rectHeight: " + rectHeight + ", screenWHRatio: " + screenWHRatio
+                    + ", rect w/h: " + (1f * rectWidth / rectHeight));
+
+            final int matrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+            Matrix.setIdentityM(mMatrixProjection, 0);
+            Log.v("Wbj", "onDrawFrame, setIdentityM: " + Arrays.toString(mMatrixProjection));
+            Matrix.orthoM(mMatrixProjection, 0, -screenWHRatio, screenWHRatio, -1f, 1f, -1f, 1f);
+            Log.v("Wbj", "onDrawFrame, orthoM: " + Arrays.toString(mMatrixProjection));
+            Matrix.translateM(mMatrixProjection, 0, 0f, -0.38f, 0f);
+            Log.v("Wbj", "onDrawFrame, translateM: " + Arrays.toString(mMatrixProjection));
+            Matrix.scaleM(mMatrixProjection, 0, 1f * rectWidth / width, 2f * rectHeight / height, 1f);
+            Log.v("Wbj", "onDrawFrame, scaleM: " + Arrays.toString(mMatrixProjection));
+            /*Matrix.orthoM(mMatrixProjection, 0, -screenWHRatio, screenWHRatio, -screenWHRatio, screenWHRatio, -1f, 1f);
+            Matrix.translateM(mMatrixProjection, 0, 0f, -0.19f, 0f);
+            Matrix.scaleM(mMatrixProjection, 0, 1f * rectWidth / width, 1f * rectHeight / height, 1f);*/
+            GLES20.glUniformMatrix4fv(matrixHandle, 1, false, mMatrixProjection, 0);
+
+            GLES20.glDrawElements(GLES20.GL_TRIANGLES, mVertexIndexBuffer.capacity(), GLES20.GL_UNSIGNED_SHORT, mVertexIndexBuffer);
+
+            GLES20.glDisableVertexAttribArray(positionHandle);
+
+            this.onDraw(width, height);
+        }
+
+        private void onDraw(int width, int height) {
+            GLES20.glUseProgram(mProgram2);
+
+            final int positionHandle = GLES20.glGetAttribLocation(mProgram2, "vPosition");
+            GLES20.glEnableVertexAttribArray(positionHandle);
+            GLES20.glVertexAttribPointer(positionHandle, 3, GLES20.GL_FLOAT, false,12, mVertexBuffer);
+
+            final int rectWidth = (int) (width / 1.5f);
+            final int rectHeight = rectWidth / 3;
+            final float screenWHRatio = 1f * width / height;
+            final float scaleX = 1f * rectWidth / width;
+            final float scaleY = 1f * rectHeight / height;
+            Log.d("Wbj", "onDraw, width: " + width + ", height: " + height + ", rectWidth: " + rectWidth
+                    + ", rectHeight: " + rectHeight + ", screenWHRatio: " + screenWHRatio);
+
+            final int matrixHandle = GLES20.glGetUniformLocation(mProgram2, "uMVPMatrix");
+            Matrix.setIdentityM(mMatrixProjection, 0);
+            Log.v("Wbj", "onDraw, setIdentityM: " + Arrays.toString(mMatrixProjection));
+            Matrix.orthoM(mMatrixProjection, 0, -screenWHRatio, screenWHRatio, -screenWHRatio, screenWHRatio, -1f, 1f);
+            //center:(0f, 0f)
+            //bottom center:(0f, -translateY)
+            //bottom right:(translateX, -translateY)
+            //bottom left:(-translateX, -translateY)
+            //top center:(0f, translateY)
+            //top right:(translateX, translateY)
+            //top left:(-translateX, translateY)
+            //left center:(-translateX, 0f)
+            //right center:(translateX, 0f)
+            final float translateX = screenWHRatio - scaleX / 2;
+            final float translateY = screenWHRatio - scaleY / 2;
+
+            Log.v("Wbj", "onDraw, translateX: " + translateX + ", translateY: " + translateY
+                    + ", scaleX: " + scaleX + ", scaleY: " + scaleY);
+            Matrix.translateM(mMatrixProjection, 0, translateX, -translateY, 0f);
+            Matrix.scaleM(mMatrixProjection, 0, scaleX, scaleY, 1f);
+            Log.v("Wbj", "onDraw, orthoM, translateM, scaleM: " + Arrays.toString(mMatrixProjection));
+            GLES20.glUniformMatrix4fv(matrixHandle, 1, false, mMatrixProjection, 0);
+
+            GLES20.glDrawElements(GLES20.GL_TRIANGLES, mVertexIndexBuffer.capacity(), GLES20.GL_UNSIGNED_SHORT, mVertexIndexBuffer);
+
+            GLES20.glDisableVertexAttribArray(positionHandle);
         }
     }
 
