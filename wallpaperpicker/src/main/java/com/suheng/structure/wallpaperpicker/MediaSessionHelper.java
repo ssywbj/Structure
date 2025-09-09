f541bd92b85de4ca6462c6d2a1549f86a7242d43
bangjie.wei <bangjie.wei@transsion.com>
2025-09-09 11:52:41 +0800

    adgagdgadgadgaeterythsr

--- wallpaperpicker/src/main/java/com/suheng/structure/wallpaperpicker/MediaSessionHelper.java
+++ wallpaperpicker/src/main/java/com/suheng/structure/wallpaperpicker/MediaSessionHelper.java
@@ -4,12 +4,13 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.media.session.MediaController;
 import android.media.session.MediaSessionManager;
-import android.os.Build;
+import android.media.session.PlaybackState;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executor;
 
@@ -31,44 +32,101 @@ public class MediaSessionHelper {
         return mSessionManager.getActiveSessions(notificationListener);
     }
 
+    public @NonNull List<MediaController> getActiveSessions() {
+        return getActiveSessions(null);
+    }
+
     public void addOnActiveSessionsChangedListener(@NonNull MediaSessionManager.OnActiveSessionsChangedListener sessionListener
             , @Nullable ComponentName notificationListener) {
         mSessionManager.addOnActiveSessionsChangedListener(sessionListener, notificationListener);
     }
 
     public void addOnSession2TokensChangedListener() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            if (mOnTokensChangedListener == null) {
-                mOnTokensChangedListener = session2Tokens -> Log.i(TAG, "onSession2TokensChanged, session2Tokens: " + session2Tokens);
-                mSessionManager.addOnSession2TokensChangedListener(mOnTokensChangedListener);
-            }
+        if (mOnTokensChangedListener == null) {
+            mOnTokensChangedListener = session2Tokens -> Log.i(TAG, "onSession2TokensChanged, session2Tokens: " + session2Tokens);
+            mSessionManager.addOnSession2TokensChangedListener(mOnTokensChangedListener);
         }
     }
 
     public void addOnMediaKeyEventSessionChangedListener(@NonNull Executor executor) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            if (mOnKeyEventChangedListener == null) {
-                mOnKeyEventChangedListener = (pkg, sessionToken) -> Log.i(TAG, "onMediaKeyEventSessionChanged, pkg: " + pkg + ", sessionToken: " + sessionToken);
-                mSessionManager.addOnMediaKeyEventSessionChangedListener(executor, mOnKeyEventChangedListener);
-            }
+        if (mOnKeyEventChangedListener == null) {
+            mOnKeyEventChangedListener = (pkg, sessionToken) -> Log.i(TAG, "onMediaKeyEventSessionChanged, pkg: " + pkg + ", sessionToken: " + sessionToken);
+            mSessionManager.addOnMediaKeyEventSessionChangedListener(executor, mOnKeyEventChangedListener);
         }
     }
 
     public void removeChangedListeners() {
         Log.d(TAG, "removeChangedListeners()");
-        /*if (mSessionListener != null) {
-            mSessionManager.removeOnActiveSessionsChangedListener(mSessionListener);
-        }*/
         if (mOnTokensChangedListener != null) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                mSessionManager.removeOnSession2TokensChangedListener(mOnTokensChangedListener);
-            }
+            mSessionManager.removeOnSession2TokensChangedListener(mOnTokensChangedListener);
         }
         if (mOnKeyEventChangedListener != null) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                mSessionManager.removeOnMediaKeyEventSessionChangedListener(mOnKeyEventChangedListener);
+            mSessionManager.removeOnMediaKeyEventSessionChangedListener(mOnKeyEventChangedListener);
+        }
+    }
+
+    public boolean existActivePlayer() {
+        return !getActiveSessions().isEmpty();
+    }
+
+    public boolean existPlayingPlayer(final boolean includeBuffering) {
+        List<MediaController> activeSessions = getActiveSessions();
+        boolean isPlaying = false;
+        for (MediaController controller : activeSessions) {
+            final PlaybackState playbackState = controller.getPlaybackState();
+            if (playbackState == null) {
+                continue;
+            }
+
+            final int state = playbackState.getState();
+            if (state == PlaybackState.STATE_PLAYING) {
+                isPlaying = true;
+                break;
+            }
+
+            if (includeBuffering && state == PlaybackState.STATE_BUFFERING) {
+                isPlaying = true;
+                break;
             }
         }
+
+        return isPlaying;
+    }
+
+    public boolean existPlayingPlayer() {
+        return existPlayingPlayer(false);
+    }
+
+    public @Nullable List<String> getPlayingPlayers(final boolean includeBuffering) {
+        List<MediaController> activeSessions = getActiveSessions();
+        List<String> pkgs = null;
+        boolean isPlaying;
+        for (MediaController controller : activeSessions) {
+            final PlaybackState playbackState = controller.getPlaybackState();
+            if (playbackState == null) {
+                continue;
+            }
+
+            isPlaying = false;
+
+            final int state = playbackState.getState();
+            if (state == PlaybackState.STATE_PLAYING) {
+                isPlaying = true;
+            }
+
+            if (includeBuffering && state == PlaybackState.STATE_BUFFERING) {
+                isPlaying = true;
+            }
+
+            if (isPlaying) {
+                if (pkgs == null) {
+                    pkgs = new ArrayList<>();
+                }
+                pkgs.add(controller.getPackageName());
+            }
+        }
+
+        return pkgs;
     }
 
 }
